c5118a27f72cb22457de0d6ea9a9f5ba09cdaf39
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2023-08-22 16:25:32 -0400

    use the same wrapper used in clara/recon-util

--- reconstruction/dc/src/test/java/org/jlab/service/dc/DCReconstructionTest.java
+++ reconstruction/dc/src/test/java/org/jlab/service/dc/DCReconstructionTest.java
@@ -50,8 +50,8 @@ public class DCReconstructionTest {
     DCHBPostClusterConv engineHB = new DCHBPostClusterConv();
     engineCL.init();
     engineHB.init();
-    engineCL.processDataEvent(testEvent); 
-    engineHB.processDataEvent(testEvent); 
+    engineCL.processEvent(testEvent); 
+    engineHB.processEvent(testEvent); 
     if(testEvent.hasBank("HitBasedTrkg::HBTracks")) {
         testEvent.getBank("HitBasedTrkg::HBTracks").show();
     }
