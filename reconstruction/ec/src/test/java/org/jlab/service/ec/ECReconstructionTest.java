8259d92a106cd5bf8457625262f978a368d686c7
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2023-08-22 15:55:34 -0400

    use the same wrapper used in clara/recon-util

--- reconstruction/ec/src/test/java/org/jlab/service/ec/ECReconstructionTest.java
+++ reconstruction/ec/src/test/java/org/jlab/service/ec/ECReconstructionTest.java
@@ -23,7 +23,7 @@ public class ECReconstructionTest {
     
     ECEngine engineEC = new ECEngine();
     engineEC.init();
-    engineEC.processDataEvent(testEvent);
+    engineEC.processEvent(testEvent);
 
     testEvent.show();
     testEvent.getBank("ECAL::hits").show();
