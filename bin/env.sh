f1d948fbe2e26bd875bb81b6c49df436a08d3644
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-09-06 18:26:32 -0400

    fix: do nothing if arg is not `groovy` or `jshell`

--- bin/env.sh
+++ bin/env.sh
@@ -16,19 +16,22 @@ fi
 # additional environment variables for groovy or interactive use
 # - call as `source $0 groovy` or `source $0 jshell`
 if [ $# -ge 1 ]; then
+  if [ "$1" == "groovy" -o "$1" == "jshell" ]; then
 
-  # add jar files to class path
-  for lib in clas services utils; do
-    for jars in $(ls -a $CLAS12DIR/lib/$lib/*.jar); do
-      JYPATH="${JYPATH:+${JYPATH}:}${jars}"
+    # add jar files to class path
+    for lib in clas services utils; do
+      for jars in $(ls -a $CLAS12DIR/lib/$lib/*.jar); do
+        JYPATH="${JYPATH:+${JYPATH}:}${jars}"
+      done
     done
-  done
 
-  # additional variables and settings for groovy
-  if [ "$1" == "groovy" ]; then
-    JYPATH="${JYPATH:+${JYPATH}:}${CLAS12DIR}/lib/packages"
-    export JAVA_OPTS="-Dsun.java2d.pmoffscreen=false -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties -Xms1024m -Xmx2048m -XX:+UseSerialGC"
+    # additional variables and settings for groovy
+    if [ "$1" == "groovy" ]; then
+      JYPATH="${JYPATH:+${JYPATH}:}${CLAS12DIR}/lib/packages"
+      export JAVA_OPTS="-Dsun.java2d.pmoffscreen=false -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties -Xms1024m -Xmx2048m -XX:+UseSerialGC"
+    fi
+
+    export JYPATH
+
   fi
-
-  export JYPATH
 fi
