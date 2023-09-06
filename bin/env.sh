c9df447e3dad905b72941d79af31e7291b9443d7
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-09-06 18:15:43 -0400

    refactor: combine environment for `run-groovy` and `run-jshell`

--- bin/env.sh
+++ bin/env.sh
@@ -13,3 +13,22 @@ if [ -z "$COAT_MAGFIELD_SOLENOIDMAP" ]; then
     export COAT_MAGFIELD_SOLENOIDMAP=Symm_solenoid_r601_phi1_z1201_13June2018.dat
 fi
 
+# additional environment variables for groovy or interactive use
+# - call as `source $0 groovy` or `source $0 jshell`
+if [ $# -ge 1 ]; then
+
+  # add jar files to class path
+  for lib in clas services utils; do
+    for jars in $(ls -a $CLAS12DIR/lib/$lib/*.jar); do
+      JYPATH="${JYPATH:+${JYPATH}:}${jars}"
+    done
+  done
+
+  # additional variables and settings for groovy
+  if [ "$1" == "groovy" ]; then
+    JYPATH="${JYPATH:+${JYPATH}:}${CLAS12DIR}/lib/packages"
+    export JAVA_OPTS="-Dsun.java2d.pmoffscreen=false -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties -Xms1024m -Xmx2048m -XX:+UseSerialGC"
+  fi
+
+  export JYPATH
+fi
