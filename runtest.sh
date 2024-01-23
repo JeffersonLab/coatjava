672b3ad774bd93b322bd06d4dcfe69c0a2aef390
ziegler <ziegler@enpziegler-mac.jlab.org>
2024-01-23 14:11:51 -0500

    version bump

---
+++ runtest.sh
@@ -0,0 +1,3 @@
+rm ../../../../../Files/CVTDEBUG/rec_cvt1.hipo
+./bin/recon-util -S etc/bankdefs/hipo4/ -i ../../../../../Files/CVTDEBUG/gen_cvt1.hipo -o ../../../../../Files/CVTDEBUG/rec_cvt1.hipo -y ../../../../../Files/CVTDEBUG/mc-fall2018-split.yaml -n 20
+
