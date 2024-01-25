5b6e440d689f392f28565cc005804ac9e6532b93
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2024-01-25 13:54:34 -0500

    add sorted hipo reader class

---
+++ common-tools/clas-io/src/main/java/org/jlab/io/hipo/HipoSortedDataSource.java
@@ -0,0 +1,110 @@
+package org.jlab.io.hipo;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+import org.jlab.io.base.DataEvent;
+import org.jlab.jnp.hipo4.data.Bank;
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.data.Schema;
+
+/**
+ *
+ * A wrapper on HipoDataSource to read one file sorted by CODA event number.
+ *
+ * WARNING:  This is strictly for reading sorted events sequentially.  Methods
+ * other than the overridden "open", "hasEvent", "getNextEvent", and "reset"
+ * may not work as intended.
+ * 
+ * FIXME:  Events with no HEAD bank or negative event number are unreachable.
+ * 
+ * @author baltzell
+ */
+public class HipoSortedDataSource extends HipoDataSource {
+    
+    // Ordered map of CODA event number to EVIO event index:
+    private final TreeMap<Integer,Integer> map = new TreeMap<>();
+
+    // Ordered list of CODA event numbers (just for map lookup
+    // by key index without creating a new object every time): 
+    private final ArrayList<Integer> list = new ArrayList<>();
+
+    // Current event index during sorted reading:
+    private int index = 0;
+
+    public HipoSortedDataSource () {
+        super();
+    }
+
+    public TreeMap<Integer, Integer> getMapping() {
+        return new TreeMap<>(this.map);
+    }
+
+    /**
+     * Read the file once and initialize the event ordering
+     * @param filename 
+     */
+    private void load(String filename, int maxEvents) {
+        Logger.getLogger(this.getClass().getName()).info("Initializing EvioSortedSource ...");
+        this.map.clear();
+        this.list.clear();
+        super.open(filename);
+        Schema s = this.reader.getSchemaFactory().getSchema("RUN::config");
+        Bank b = new Bank(s);
+        while (super.hasEvent()) {
+            if (maxEvents>0 && this.map.keySet().size()>maxEvents) break;
+            Event e = (Event)super.getNextEvent();
+            if (e.hasBank(s)) {
+                e.read(b);
+                if (b.getRows() > 0) {
+                    int coda = b.getInt("event",0);
+                    if (coda > 0) {
+                        int evio = this.currentEventNumber;
+                        map.put(coda, evio);
+                    }
+                }
+            }
+        }
+        this.close();
+        this.list.addAll(new ArrayList(this.map.keySet()));
+    }
+
+    /**
+     * Initialize the event ordering and (re)open the file
+     * @param filename 
+     */
+    @Override
+    public void open(String filename) {
+        this.load(filename, -1);
+        this.index = 0;
+        super.open(filename);
+    }
+
+    /**
+     * @return whether there's events remaining
+     */
+    @Override
+    public boolean hasEvent() {
+        return this.index < this.list.size()-1;
+    }
+    
+    /**
+     * Retrieve the next event, as sorted by CODA event number
+     * @return event 
+     */
+    @Override
+    public DataEvent getNextEvent() {
+        final int coda = this.list.get(this.index);
+        final int evio = this.map.get(coda);
+        this.index++;
+        return this.gotoEvent(evio);
+    }
+
+    /**
+     * Go back to the "beginning" of the file
+     */
+    @Override
+    public void reset() {
+        this.index = 0;
+    }
+}
