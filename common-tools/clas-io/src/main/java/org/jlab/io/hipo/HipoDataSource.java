d91af4f20c507d3d92c20272a65baf6aab853b7e
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2024-01-25 17:21:50 -0500

    add direct event access, cleanup

--- common-tools/clas-io/src/main/java/org/jlab/io/hipo/HipoDataSource.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/hipo/HipoDataSource.java
@@ -9,7 +9,6 @@ import org.jlab.io.base.DataEvent;
 import org.jlab.io.base.DataEventList;
 import org.jlab.io.base.DataEventType;
 import org.jlab.io.base.DataSource;
-import org.jlab.io.evio.EvioDataDictionary;
 import org.jlab.io.base.DataSourceType;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.data.SchemaFactory;
@@ -24,12 +23,8 @@ public class HipoDataSource implements DataSource {
 
     public static Logger LOGGER = Logger.getLogger(HipoDataSource.class.getName());
 
-    HipoReader  reader = null;
-    EvioDataDictionary  dictionary = new EvioDataDictionary();
-    int                 numberOfRecords = 0;
-    int                 currentEventNumber = 0;
-    int                 minEventNumber     = 0;
-    int                 numberOfEvent      = 0;
+    HipoReader reader = null;
+    int currentEventIndex = 0;
     
     public HipoDataSource(){
         this.reader = new HipoReader();
@@ -44,6 +39,7 @@ public class HipoDataSource implements DataSource {
     public void open(File file) {
         this.open(file.getAbsolutePath());
     }
+
     /**
      * Creates a Writer class with Dictionary from the Reader.
      * This method should be used when filtering the input file
@@ -58,28 +54,13 @@ public class HipoDataSource implements DataSource {
     
     @Override
     public void open(String filename) {
+        this.currentEventIndex = 0;
         this.reader.open(filename);
         LOGGER.log(Level.INFO,"[DataSourceDump] --> opened file with events # " );
-        //this.reader.getSchemaFactory().show();
-        /*
-        HipoRecord header = this.reader.getHeaderRecord();
-        int  ncount = header.getEventCount();
-        System.out.println("[HipoDataSource] ---> dictionary record opened. # entries = " + ncount);
-        for(int ev = 0; ev < ncount; ev++){
-            byte[] descBytes  = header.getEvent(ev);
-            String descString = new String(descBytes);
-            //System.out.println("init dictionary : " + descString);
-            EvioDataDescriptor  descriptor = new EvioDataDescriptor(descString);
-            this.dictionary.addDescriptor(descriptor);
-        }*/
-        //this.dictionary.show();
-        /*this.minEventNumber = 0;
-        this.currentEventNumber = 0;
-        this.numberOfEvent      = this.reader.getEventCount();*/
     }
 
     public void open(ByteBuffer buff) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void close() {
@@ -89,46 +70,35 @@ public class HipoDataSource implements DataSource {
     public  HipoReader getReader(){ return reader;}
     @Override
     public int getSize() {
-        //return reader.
-        //return 1;
         return reader.getEventCount();
     }
 
     @Override
     public DataEventList getEventList(int start, int stop) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public DataEventList getEventList(int nrecords) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public DataEvent getNextEvent() {
         Event event = new Event();
         reader.nextEvent(event);
-        /*hipoEvent.getDataBuffer();        
-        System.out.println(" GET NEXT HIPO EVENT : DICTIONARY SIZE = " + 
-                hipoEvent.getSchemaFactory().getSchemaList().size() + "  EVENT LENGTH = "
-                + hipoEvent.getDataBuffer().length);
-        hipoEvent.showNodes();*/
         HipoDataEvent  hipoEvent = new HipoDataEvent(event,reader.getSchemaFactory());
         if(reader.hasNext()==true){
             hipoEvent.setType(DataEventType.EVENT_ACCUMULATE);
         } else {
             hipoEvent.setType(DataEventType.EVENT_STOP);
         }
+        this.currentEventIndex++;
         return hipoEvent;
     }
 
     @Override
     public DataEvent getPreviousEvent() {
-       
-        /*HipoEvent  hipoEvent = reader.readPreviousEvent();
-        hipoEvent.getDataBuffer();
-        HipoDataEvent  evioEvent = new HipoDataEvent(hipoEvent.getDataBuffer(),hipoEvent.getSchemaFactory());*/
-        //return hipoEvent;
         return null;
     }
 
@@ -136,11 +106,6 @@ public class HipoDataSource implements DataSource {
     public DataEvent gotoEvent(int index) {
         Event event = new Event();
         reader.getEvent(event, index);
-        /*hipoEvent.getDataBuffer();        
-        System.out.println(" GET NEXT HIPO EVENT : DICTIONARY SIZE = " + 
-                hipoEvent.getSchemaFactory().getSchemaList().size() + "  EVENT LENGTH = "
-                + hipoEvent.getDataBuffer().length);
-        hipoEvent.showNodes();*/
         HipoDataEvent  hipoEvent = new HipoDataEvent(event,reader.getSchemaFactory());
         if(reader.hasNext()==true){
             hipoEvent.setType(DataEventType.EVENT_ACCUMULATE);
@@ -152,12 +117,12 @@ public class HipoDataSource implements DataSource {
     
     @Override
     public void reset() {
-        this.currentEventNumber = 0;
+        this.currentEventIndex = 0;
     }
 
     @Override
     public int getCurrentIndex() {
-        return this.currentEventNumber;
+        return this.currentEventIndex;
     }
         
     public static void main(String[] args){
@@ -175,7 +140,6 @@ public class HipoDataSource implements DataSource {
     @Override
     public DataSourceType getType() {
         return DataSourceType.FILE;        
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
