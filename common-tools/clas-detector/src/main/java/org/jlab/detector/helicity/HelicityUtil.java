e6d8b9d35bc2a8cd032a7646487cd0c2cd63473a
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2024-01-24 20:03:40 -0500

    cleanup

--- common-tools/clas-detector/src/main/java/org/jlab/detector/helicity/HelicityUtil.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/helicity/HelicityUtil.java
@@ -2,46 +2,51 @@ package org.jlab.detector.helicity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 import org.jlab.detector.decode.CLASDecoder4;
 import org.jlab.detector.decode.EvioSortedSource;
 import org.jlab.io.base.DataEvent;
 import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.utils.benchmark.ProgressPrintout;
 
 /**
  *
  * @author baltzell
  */
 public class HelicityUtil {
-    
+
     /**
-     * This is the helicity flip detection extracted from the standard EVIO-HIPO
-     * decoder, CLASDecoder4, to facilitate decoding of data with EVIO event
-     * misordering without changing their original order.
+     * This is just the helicity flip detection extracted from the standard
+     * decoder, CLASDecoder4, to facilitate using that same decoder on misordered
+     * EVIO events, without changing their original order in the output HIPO file.
      * 
      * The original approach added helicity flip information to events on-the-fly
      * as the flips were detected.  Here, instead all events are first read and
-     * sorted before flip detection.  This does imply reading all the events
-     * twice, once here to get the ordering, and then again later to do the data
-     * format conversion and write to a file.
-     *
-     * As is currently, this is very inefficient, due to reading far more raw
-     * detector data than necessary for flip detection.  A first look suggests
-     * that fixing that, without copy-pasting low-level code around, while 
-     * still supporting all the FADC readout modes, would be a significant project
-     * and time would be better spent on a larger rewrite, which is already in
-     * progress but not ready to support the entire CLAS12 data.
-     * 
-     * Anyway, in the big picture the CPU time required is still very small
-     * relative to reconstruction.
+     * sorted, which isn't too much overhead because that only requires reading
+     * event headers.  All events are then read again for helicity flip detection,
+     * which is currently very inefficient due to reading far more raw detector
+     * data than necessary for flip detection, due to reusing functionality from
+     * the decoder.  A first look suggests that fixing that, without copy-pasting
+     * low-level code around, while still supporting all the FADC readout modes,
+     * would be a significant project and time would be better spent on a larger
+     * rewrite, which is already in independently progress but not ready to
+     * support the entire CLAS12 data.
      * 
      * Alternatively, flip detection could operate as a post-processor on a HIPO
-     * file, but it looks like the HIPO reader used in the decoder does not
-     * support direct, non-sequential event access.
+     * file, which should be somewhat more efficient, but it looks like the HIPO
+     * reader used in the decoder does not support direct, non-sequential event
+     * access.
      * 
+     * Meanwhile, in the big picture, this essentially doubles the CPU time
+     * required for decoding, which is still a ~1 % effect on the total.
+     * 
+     * @param s
      * @return list of helicity flips' corresponding HEL::flip and RUN::config banks
      */
     public static List<Event> getFlips(EvioSortedSource s) {
+        ProgressPrintout progress = new ProgressPrintout();
+        Logger.getLogger(HelicityUtil.class.getName()).info("Initializing Helicity Flips");
         List<Event> flips = new ArrayList<>();
         CLASDecoder4 d = new CLASDecoder4();
         Bank adc = new Bank(d.getSchemaCopy("HEL::adc"));
@@ -51,6 +56,7 @@ public class HelicityUtil {
             DataEvent evio = s.getNextEvent();
             Event hipo = d.getDataEvent(evio);
             hipo.read(adc);
+            hipo.read(cfg);
             if (adc.getRows()>0) {
                 HelicityState thisHelicity = HelicityState.createFromFadcBank(adc);
                 if (!thisHelicity.isValid() || !thisHelicity.equals(prevHelicity)) {
@@ -61,6 +67,7 @@ public class HelicityUtil {
                     flips.add(e);
                     prevHelicity = thisHelicity;
                 }
+                progress.updateStatus();
             }
                 
         }
