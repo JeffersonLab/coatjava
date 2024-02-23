66366c6d37094673f69eadcd439884a55620fa21
Nathan Baltzell <baltzell@macbook-pro.mynetworksettings.com>
2024-02-22 19:18:53 -0500

    bump version

--- common-tools/coat-lib/deployDistribution.sh
+++ common-tools/coat-lib/deployDistribution.sh
@@ -8,7 +8,7 @@ cd `dirname $0`
 # Script is exporting existing Jar files to repository
 #-------------------------------------------------------------------------------------------------
 
-VERSION=10.0.7
+VERSION=10.0.8
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file \
     -Dfile=target/coat-libs-${VERSION}-SNAPSHOT.jar \
