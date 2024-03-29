056fc8d1389e91ed424fc360acf746c69bba2931
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2024-03-29 15:26:56 -0400

    remove embedded dependency

--- build-coatjava.sh
+++ build-coatjava.sh
@@ -92,11 +92,7 @@ cp -r etc coatjava/
 which python3 >& /dev/null && python=python3 || python=python
 $python etc/bankdefs/util/bankSplit.py coatjava/etc/bankdefs/hipo4 || exit 1
 mkdir -p coatjava/lib/clas
-#cp external-dependencies/JEventViewer-1.1.jar coatjava/lib/clas/
-#cp external-dependencies/vecmath-1.3.1-2.jar coatjava/lib/clas/
 mkdir -p coatjava/lib/utils
-cp external-dependencies/jclara-4.3-SNAPSHOT.jar coatjava/lib/utils
-#cp external-dependencies/jaw-1.0.jar coatjava/lib/utils
 mkdir -p coatjava/lib/services
 
 ### clean up any cache copies ###
