b0af70bf28d9d65c031019a8aaa62a4a71a0275a
tongtongcao <tongtongcao1@gmail.com>
2024-04-01 14:38:26 -0400

    update iteration termination coditions

--- common-tools/clas-tracking/src/main/java/org/jlab/clas/tracking/kalmanfilter/zReference/Constants.java
+++ common-tools/clas-tracking/src/main/java/org/jlab/clas/tracking/kalmanfilter/zReference/Constants.java
@@ -30,13 +30,13 @@ public class Constants {
     public static final double ITERSTOPQTB = 1.1e-5;       
 
     
-    public static final double ITERSTOPXHBSTR4PARAS = 1.3e-06; 
-    public static final double ITERSTOPYHBSTR4PARAS = 3.3e-5;  
-    public static final double ITERSTOPTXHBSTR4PARAS = 6.5e-9;  
-    public static final double ITERSTOPTYHBSTR4PARAS = 1.6e-7;  
+    public static final double ITERSTOPXHBSTR4PARAS = 6.0e-5; 
+    public static final double ITERSTOPYHBSTR4PARAS = 3.9e-3;  
+    public static final double ITERSTOPTXHBSTR4PARAS = 3.3e-7;  
+    public static final double ITERSTOPTYHBSTR4PARAS = 2.2e-5;  
     
-    public static final double ITERSTOPXTBSTR4PARAS = 5.5e-5; 
-    public static final double ITERSTOPYTBSTR4PARAS = 5.3e-4;  
-    public static final double ITERSTOPTXTBSTR4PARAS = 5.1e-8;  
-    public static final double ITERSTOPTYTBSTR4PARAS = 5.0e-7;    
+    public static final double ITERSTOPXTBSTR4PARAS = 3.6e-4; 
+    public static final double ITERSTOPYTBSTR4PARAS = 3.4e-3;  
+    public static final double ITERSTOPTXTBSTR4PARAS = 4.4e-6;  
+    public static final double ITERSTOPTYTBSTR4PARAS = 4.1e-5;    
 }
