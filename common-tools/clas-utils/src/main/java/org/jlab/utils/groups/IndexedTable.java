0f7c95bb9696b01a9cd78a8cdf83b3748fbb79fa
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2024-04-02 10:46:37 -0400

    cleanup

--- common-tools/clas-utils/src/main/java/org/jlab/utils/groups/IndexedTable.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/groups/IndexedTable.java
@@ -331,22 +331,21 @@ public class IndexedTable extends DefaultTableModel {
     public static class IndexedCellRenderer extends DefaultTableCellRenderer {
         @Override
         public Component getTableCellRendererComponent
-                (JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
-                {
-                    Component c = super.getTableCellRendererComponent
-                                          (table, value, isSelected, hasFocus, row, column);
-                     if(isSelected==true){
-                         c.setBackground(new Color(20,20,255));                
-                         return c;
-                     }
-                    if(row%2==0){
-                        c.setBackground(new Color(220,255,220));
-                    } else {
-                        c.setBackground(new Color(220,220,255));
-                    }
+                (JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+            Component c = super.getTableCellRendererComponent
+                          (table, value, isSelected, hasFocus, row, column);
+            if(isSelected==true){
+                c.setBackground(new Color(20,20,255));                
+                return c;
+            }
+            if(row%2==0){
+                c.setBackground(new Color(220,255,220));
+            } else {
+                c.setBackground(new Color(220,220,255));
+            }
                     
-                    return c;
-                }
+            return c;
+        }
     }
     
     public static class IndexedEntry {
