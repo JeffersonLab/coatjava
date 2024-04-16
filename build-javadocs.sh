590998101b4f1e61f55f45f7902c4d35b1833671
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-04-16 18:39:51 -0400

    feat: generate front page

--- build-javadocs.sh
+++ build-javadocs.sh
@@ -5,8 +5,11 @@
 
 set -e
 
-mvn javadoc:javadoc -Ddoclint=none 
+##### generate documentation
+mvn javadoc:javadoc -Ddoclint=none
 
+
+##### collect documentation
 src=target/site/apidocs
 dest=docs/javadoc
 
@@ -16,5 +19,39 @@ do
     cp -r $dir/* $dest/${dir%$src}
 done
 
-#scp -r docs/javadoc/* ifarm:/group/clas/www/clasweb/html/clas12offline/docs/javadoc
 
+##### generate front index page
+pushd $dest
+
+cat > index.html << EOF
+<html><body bgcolor="cccccc"><head>
+<title>coatjava javadocs</title>
+<STYLE TYPE="text/css"></STYLE>
+</head>
+<p><p align="center"><b><font size="5">CLAS12 Coatjava Javadocs <br></b></font></p>
+<br>
+<br>
+EOF
+
+pages=($(find -name "index.html" | sed 's;^\./;;' | sed 's;/index.html;;' | grep -v index.html | sort))
+header=""
+for page in ${pages[@]}; do
+  headerTmp=$(echo $page | sed 's;/.*;;g')
+  obj=$(echo $page | sed "s;^$headerTmp/;;")
+  if [ "$header" != "$headerTmp" ]; then
+    [ "$header" != "" ] && echo "</ul>" >> index.html
+    header=$headerTmp
+    echo "<h3>$header</h3>" >> index.html
+    echo "<ul>" >> index.html
+  fi
+  echo "<li><a href=\"$page/index.html\">$obj</a></li>" >> index.html
+done
+
+cat >> index.html << EOF
+</ul>
+</body>
+</html>
+EOF
+
+popd
+echo "Documentation generated: $dest/index.html"
