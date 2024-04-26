a7cce0163bdbf5858e94656c30c72c18b1b01626
heddle <David.Heddle@cnu.edu>
2024-04-26 10:59:54 -0400

    starting new converter for new transverse solenoid

---
+++ common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/converter/ConvertTransverse.java
@@ -0,0 +1,13 @@
+package cnuphys.magfield.converter;
+
+/**
+ * Convert a new transverse map given April 2024
+ */
+public class ConvertTransverse {
+
+	public static void main(String arg[]) {
+		String homeDir = System.getProperty("user.home");
+	
+	}
+
+}
