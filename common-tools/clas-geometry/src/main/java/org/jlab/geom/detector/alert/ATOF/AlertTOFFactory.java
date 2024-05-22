11d695436c95a3b0e131fa7aa54bd93629e43d2a
Nathan Baltzell <baltzell@jlab.org>
2024-05-22 17:20:29 -0400

    cleanup

--- common-tools/clas-geometry/src/main/java/org/jlab/geom/detector/alert/ATOF/AlertTOFFactory.java
+++ common-tools/clas-geometry/src/main/java/org/jlab/geom/detector/alert/ATOF/AlertTOFFactory.java
@@ -1,11 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//package clas12vis;
-
 package org.jlab.geom.detector.alert.ATOF;
 
 import org.jlab.geom.base.ConstantProvider;
@@ -15,9 +7,6 @@ import org.jlab.geom.component.ScintillatorPaddle;
 import org.jlab.geom.prim.Plane3D;
 import org.jlab.geom.prim.Point3D;
 import org.jlab.geom.prim.Transformation3D;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author viktoriya
