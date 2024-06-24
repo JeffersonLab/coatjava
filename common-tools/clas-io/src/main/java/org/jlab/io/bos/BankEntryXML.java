6f3833606a7363538577d08ae37d6ee77b8fa551
Nathan Baltzell <baltzell@jlab.org>
2024-06-24 10:10:47 -0400

    api moved to different packages

--- common-tools/clas-io/src/main/java/org/jlab/io/bos/BankEntryXML.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/bos/BankEntryXML.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jlab.io.bos;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 /**
  *
