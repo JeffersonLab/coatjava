db8c28f4c017835129909c075dbd2856dc568e15
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-08-12 15:59:12 -0400

    fix: unknown `-l`

--- validation/advanced-tests/run-advanced-tests.sh
+++ validation/advanced-tests/run-advanced-tests.sh
@@ -15,7 +15,7 @@ mv coatjava-local.tar.gz validation/advanced-tests/
 cd -
 
 # install clara
-../../install-clara -f 5.0.2 -j 11 -l $CLARA_HOME
+../../install-clara -f 5.0.2 -j 11 $CLARA_HOME
 if [ $? != 0 ] ; then echo "clara installation error" ; exit 1 ; fi
 
 # download test files
