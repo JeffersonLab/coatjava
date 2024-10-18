7da710989ded7be76ec77e0b60ca63e9ab919e25
Nathan Baltzell <baltzell@jlab.org>
2024-10-18 14:19:28 -0400

    resolve merge conflict

--- reconstruction/dc/src/main/java/org/jlab/service/dc/DCTBEngine.java
+++ reconstruction/dc/src/main/java/org/jlab/service/dc/DCTBEngine.java
@@ -70,7 +70,7 @@ public class DCTBEngine extends DCEngine {
     }
     
     @Override
-    public boolean processDataEvent(DataEvent event) {
+    public boolean processDataEventUser(DataEvent event) {
         int run = this.getRun(event);
         if(run==0) return true;
         
