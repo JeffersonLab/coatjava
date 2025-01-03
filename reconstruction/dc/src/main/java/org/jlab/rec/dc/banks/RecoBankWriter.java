c5caeb21ac63c930235770493b1902f4a59c202c
Raffaella De Vita <devita@ge.infn.it>
2025-01-03 17:37:45 -0500

    accounting for tstart, tflight and tprop in MC tracks reconstruction

--- reconstruction/dc/src/main/java/org/jlab/rec/dc/banks/RecoBankWriter.java
+++ reconstruction/dc/src/main/java/org/jlab/rec/dc/banks/RecoBankWriter.java
@@ -570,7 +570,7 @@ public DataBank fillHBClustersBank(DataEvent event, List<FittedCluster> cluslist
             if(bank.getDescriptor().hasEntry("beta")){
                bank.setFloat("beta", i, (float) hitlist.get(i).get_Beta());
             }
-            if(hitlist.get(i).get_AssociatedTBTrackID()>-1 && !event.hasBank("MC::Particle")) {
+            if(hitlist.get(i).get_AssociatedTBTrackID()>-1) {
                 if(hitlist.get(i).getSignalPropagTimeAlongWire()==0 || hitlist.get(i).get_AssociatedTBTrackID()<1) {
                     bank.setFloat("TProp", i, (float) hitlist.get(i).getTProp()); //old value if track fit failed
                 } else {
