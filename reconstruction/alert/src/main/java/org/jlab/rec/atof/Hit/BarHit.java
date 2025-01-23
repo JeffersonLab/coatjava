786488bb7db019333edbd6e6d05afb6e32872f84
N-Plx <plxn@tuta.io>
2025-01-23 11:43:42 -0600

    Fixing time/energy computation and some cleanup

--- reconstruction/alert/src/main/java/org/jlab/rec/atof/Hit/BarHit.java
+++ reconstruction/alert/src/main/java/org/jlab/rec/atof/Hit/BarHit.java
@@ -1,4 +1,5 @@
 package org.jlab.rec.atof.hit;
+
 import org.jlab.detector.calib.utils.DatabaseConstantProvider;
 import org.jlab.geom.base.Detector;
 import org.jlab.geom.detector.alert.ATOF.AlertTOFFactory;
@@ -12,11 +13,10 @@ import org.jlab.rec.atof.constants.Parameters;
  *
  * Represents a hit in the atof bar. Extends class AtofHit. Is further defined
  * by the two hits upstream and downstream composing a full bar hit. z position,
- * time and energy are defined from the up/down hits
+ * time and energy are defined from the up/down hits.
  *
- * @authors npilleux,churaman 
+ * @author npilleux
  */
-
 public class BarHit extends AtofHit {
 
     //A bar hit is the combination of a downstream and upstream hits
@@ -38,16 +38,40 @@ public class BarHit extends AtofHit {
         this.hit_down = hit_down;
     }
 
+    /**
+     * Computes bar hit z coordinate from up/downstream hit times.
+     * 
+     */
     public final void computeZ() {
-        double some_calibration = 10; //Here read the calibration DB
-        this.setZ(some_calibration * (hit_up.getTime() - hit_down.getTime()));
+        this.setZ(Parameters.VEFF/2. * (hit_up.getTime() - hit_down.getTime()));
     }
 
+    /**
+     * Computes bar hit time from up/downstream hit times.
+     * The time is set as the time of the most energetic hit.
+     * It is corrected for propagation time.
+     * 
+     */
     public final void computeTime() {
-        double some_calibration = 10; //Here read the calibration DB
-        this.setTime(some_calibration + ((hit_up.getTime() + hit_down.getTime()) / 2.));
+        //We pick the most energetic signal as the timing signal
+        double time_at_sipm, distance_to_sipm;
+        if(this.hit_down.getEnergy() > this.hit_up.getEnergy()) {
+            time_at_sipm = this.hit_down.getTime();
+            distance_to_sipm = Parameters.LENGTH_ATOF/2. - this.getZ();
+        }
+        else {
+            time_at_sipm = this.hit_up.getTime();
+            distance_to_sipm = Parameters.LENGTH_ATOF/2. + this.getZ();
+        }
+        this.setTime(time_at_sipm - distance_to_sipm/Parameters.VEFF);
     }
 
+    /**
+     * Computes bar hit energy from up/downstream hits.
+     * The energy of the up/downstream hits is corrected for attenuation now that z is known.
+     * The energy of the bar hit is the sum of the energy of the up/downstream hits.
+     * 
+     */
     public final void computeEnergy() {
         this.computeZ();
         double distance_hit_to_sipm_up = Parameters.LENGTH_ATOF / 2. + this.getZ();
@@ -68,6 +92,7 @@ public class BarHit extends AtofHit {
         this.hit_down = hit_down;
         this.setLayer(hit_up.getLayer());
         this.setSector(hit_up.getSector());
+        this.setComponent(10);
         this.setX(hit_up.getX());
         this.setY(hit_up.getY());
         this.computeZ();
@@ -76,65 +101,15 @@ public class BarHit extends AtofHit {
     }
 
     public BarHit() {
+        super();  // Call AtofHit constructor
+        //Sets some parameters to make a bar type hit
+        this.setType("bar");
+        this.setOrder(2);
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        AlertTOFFactory factory = new AlertTOFFactory();
-        DatabaseConstantProvider cp = new DatabaseConstantProvider(11, "default");
-        Detector atof = factory.createDetectorCLAS(cp);
-
-        //Input to be read
-        String input = "/Users/npilleux/Desktop/alert/atof-reconstruction/coatjava/reconstruction/alert/src/main/java/org/jlab/rec/atof/Hit/test_tdc_atof.hipo";
-        HipoDataSource reader = new HipoDataSource();
-        reader.open(input);
-
-        int event_number = 0;
-        while (reader.hasEvent()) {
-            DataEvent event = (DataEvent) reader.getNextEvent();
-            event_number++;
-            DataBank bank = event.getBank("ATOF::tdc");
-            int nt = bank.rows(); // number of tracks 
-            ArrayList<AtofHit> hit_up = new ArrayList<>();
-            ArrayList<AtofHit> hit_down = new ArrayList<>();
-            ArrayList<AtofHit> hit_wedge = new ArrayList<>();
-            for (int i = 0; i < nt; i++) {
-                int sector = bank.getInt("sector", i);
-                int layer = bank.getInt("layer", i);
-                int component = bank.getInt("component", i);
-                int order = bank.getInt("order", i);
-                int tdc = bank.getInt("TDC", i);
-                int tot = bank.getInt("ToT", i);
-                AtofHit hit = new AtofHit(sector, layer, component, order, tdc, tot, atof);
-                if (null == hit.getType()) {
-                    System.out.print("Undefined hit type \n");
-                } else {
-                    switch (hit.getType()) {
-                        case "bar up" ->
-                            hit_up.add(hit);
-                        case "bar down" ->
-                            hit_down.add(hit);
-                        case "wedge" ->
-                            hit_wedge.add(hit);
-                        default ->
-                            System.out.print("Undefined hit type \n");
-                    }
-                }
-            }
-            ArrayList<BarHit> bar_hits = new ArrayList<>();
-            for (int i_up = 0; i_up < hit_up.size(); i_up++) {
-                AtofHit this_hit_up = hit_up.get(i_up);
-                for (int i_down = 0; i_down < hit_down.size(); i_down++) {
-                    AtofHit this_hit_down = hit_down.get(i_down);
-                    if (this_hit_up.matchBar(this_hit_down)) {
-                        BarHit this_bar_hit = new BarHit(this_hit_up, this_hit_down);
-                        bar_hits.add(this_bar_hit);
-                    }
-                }
-            }
-        }
     }
 }
