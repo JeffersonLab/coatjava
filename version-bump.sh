73747d692070435955a9d1e0e92f4a7e3c23806d
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-03-31 10:43:26 -0400

    fix: re-bump to 12.0.1t

--- version-bump.sh
+++ version-bump.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 
 main_branch=development
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
   echo """USAGE: $0 [NEW_VERSION_NUMBER]
+
   BEFORE RUNNING:
   - [ ] be on branch '$main_branch' before doing this, and it should
         be up-to-date with the remote (run 'git pull')
@@ -13,25 +14,39 @@ if [ $# -ne 1 ]; then
   EFFECT:
   - a new git branch for this version bump will be created, and a commit will be added
   - at the end, all you have to do is run 'git push' and open a pull request
+
+  OPTION: if you want to just run the version bump without involving 'git' in any way,
+          add the argument '--no-git'; this will ONLY bump the version numbers
   """
   exit 2
 fi
+ver_arg=$1
+[ $# -ge 2 -a "${2-}" = "--no-git" ] && use_git=false || use_git=true
 
 # parse argument
-ver_num=$(echo $1|sed 's/-SNAPSHOT//g')  # remove '-SNAPSHOT', if the user included it
-ver_pom=$ver_num-SNAPSHOT                # append '-SNAPSHOT' for POM files
+ver_num=$(echo $ver_arg|sed 's/-SNAPSHOT//g')  # remove '-SNAPSHOT', if the user included it
+ver_pom=$ver_num-SNAPSHOT                      # append '-SNAPSHOT' for POM files
 
-# verify user is on the main branch
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$current_branch" != "$main_branch" ]; then
-  echo """ERROR: you are currently on branch '$current_branch', but you should be on branch '$main_branch'.
-  Please switch to branch '$main_branch' and (preferrably) run 'git pull'.""" >&2
-  exit 1
+# print arguments
+echo """>>>>>
+ver_num: $ver_num
+ver_pom: $ver_pom
+use_git: $use_git
+<<<<<"""
+
+# if using git, make a new branch
+if $use_git; then
+  # verify user is on the main branch
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  if [ "$current_branch" != "$main_branch" ]; then
+    echo """ERROR: you are currently on branch '$current_branch', but you should be on branch '$main_branch'.
+    Please switch to branch '$main_branch' and (preferrably) run 'git pull'.""" >&2
+    exit 1
+  fi
+  # switch to a new branch for this new version
+  new_branch=version/$ver_num
+  git switch -c $new_branch
 fi
-
-# switch to a new branch for this new version
-new_branch=version/$ver_num
-git switch -c $new_branch
 
 # bump the POM project version
 mvn --batch-mode release:update-versions -DdevelopmentVersion=$ver_pom
@@ -39,14 +54,18 @@ mvn --batch-mode release:update-versions -DdevelopmentVersion=$ver_pom
 # bump `deployDistribution.sh`'s version
 sed -i "s/^VERSION=.*/VERSION=$ver_num/g" common-tools/coat-lib/deployDistribution.sh
 
-# commit to git
-echo """
-============================================"""
-git commit -am "build: bump version number to $ver_num"
-echo """Done.
-Now run your usual 'git push' command, which is probably:
+# commit to git (or not)
+if $use_git; then
+  echo """
+  ============================================"""
+  git commit -am "build: bump version number to $ver_num"
+  echo """Done.
+  Now run your usual 'git push' command, which is probably:
 
-  git push -u origin $new_branch
+    git push -u origin $new_branch
 
-============================================
-"""
+  ============================================
+  """
+else
+  echo "Done bumping version number; no git commit was created"
+fi
