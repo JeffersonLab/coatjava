040a003ab80e900039eafdff09d9757b07a31558
Nathan Baltzell <baltzell@jlab.org>
2025-04-07 14:21:35 -0400

    fix indentation

--- common-tools/clas-detector/src/main/java/org/jlab/detector/pulse/HipoExtractor.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/pulse/HipoExtractor.java
@@ -129,8 +129,8 @@ public abstract class HipoExtractor implements IExtractor {
         for (int i=0; i<wfBank.rows(); ++i) {
             for (int j=0; j<n; ++j)
                 samples[j] = wfBank.getShort(String.format("s%d",j+1), i);
-	    long timestamp = wfBank.getLong("timestamp",i);
-	    int time = wfBank.getInt("time",i);
+            long timestamp = wfBank.getLong("timestamp",i);
+            int time = wfBank.getInt("time",i);
             List<Pulse> p = it==null ? extract(null, i, timestamp, time, samples) :
                 extract(it.getNamedEntry(getIndices(wfBank,i)), i, timestamp, time, samples);
             if (p!=null && !p.isEmpty()) {
@@ -149,8 +149,8 @@ public abstract class HipoExtractor implements IExtractor {
                 samples[j] = wfBank.getShort(String.format("s%d",j+1), i);
                 // FIXME:  Can speed this up (but looks like not for DataBank?):
                 //samples[j] = wfBank.getShort(String.format(5+j,j+1), i);
-	    int time = wfBank.getInt("time",i);
-	    long timestamp = wfBank.getLong("timestamp",i);
+            int time = wfBank.getInt("time",i);
+            long timestamp = wfBank.getLong("timestamp",i);
             List p = it==null ? extract(null, i, timestamp, time, samples) :
                 extract(it.getNamedEntry(getIndices(wfBank,i)), i, timestamp, time, samples);
             if (p!=null && !p.isEmpty()) {
