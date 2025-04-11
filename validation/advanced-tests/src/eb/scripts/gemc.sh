073224d6a200e70a48a21cd1470c598abc7b4f03
Nathan Baltzell <baltzell@gmx.com>
2025-04-11 11:07:53 -0400

    fix RNG seed

--- validation/advanced-tests/src/eb/scripts/gemc.sh
+++ validation/advanced-tests/src/eb/scripts/gemc.sh
@@ -6,6 +6,7 @@ run=11
 gemc=dev
 nevents=100
 particles=()
+seed=1234567
 
 while getopts "g:n:c:p:mdh" o
 do
@@ -53,7 +54,7 @@ function run_gemc () {
     [ -e "$_output" ] && echo Output file already exists:  $_output && exit 3
     [ -z ${dryrun+x} ] && set -x
     $dryrun gemc \
-        $_gcard -RUNNO=$_run -USE_GUI=0 -N=$_nevents \
+        $_gcard -RANDOM $seed -RUNNO=$_run -USE_GUI=0 -N=$_nevents \
         -SAVE_ALL_MOTHERS=1 -SKIPREJECTEDHITS=1 -INTEGRATEDRAW="*" -NGENP=50 \
         -INPUT_GEN_FILE="LUND, $_input" \
         -OUTPUT="hipo, $_output" &
