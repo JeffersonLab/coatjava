21041239cf079a010faf075b02b7283fa6463cef
baltzell <baltzell@gmx.com>
2025-04-11 16:29:36 -0400

    add an rg-l single-event test

---
+++ reconstruction/alert/src/test/java/org/jlab/service/alert/AlertTest.java
@@ -0,0 +1,42 @@
+package org.jlab.service.alert;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.jlab.io.base.DataEvent;
+import org.jlab.detector.base.DetectorType;
+import org.jlab.jnp.hipo4.data.SchemaFactory;
+import org.jlab.logging.DefaultLogger;
+import org.jlab.utils.system.ClasUtilsFile;
+import org.jlab.analysis.physics.TestEvent;
+
+/**
+ *
+ * @author baltzell
+ */
+public class AlertTest {
+	
+  @Test
+  public void testECReconstruction() {
+    DefaultLogger.debug();
+    System.setProperty("CLAS12DIR", "../../");
+    String dir = ClasUtilsFile.getResourceDir("CLAS12DIR", "etc/bankdefs/hipo4");
+    SchemaFactory schemaFactory = new SchemaFactory();
+    schemaFactory.initFromDirectory(dir);
+    
+    DataEvent event = TestEvent.get(DetectorType.ECAL);
+    
+    ALERTEngine engine = new ALERTEngine();
+    engine.init();
+    engine.processDataEvent(event);
+
+    event.show();
+    event.getBank("ECAL::hits").show();
+    event.getBank("ECAL::clusters").show();
+    
+    assertEquals(event.hasBank("FAKE::Bank"), false);
+    assertEquals(event.hasBank("AHDC::wf"), true);
+    assertEquals(event.getBank("AHDC::hits").rows(), 3);    
+  }
+
+}
