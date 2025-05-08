df0454ed6e9f1e031d542dafe758052ce9b21a46
Nathan Baltzell <baltzell@jlab.org>
2025-05-08 18:48:45 -0400

    add EVIO CLARA source

---
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/EvioToEvioSource.java
@@ -0,0 +1,51 @@
+package org.jlab.io.clara;
+
+import java.nio.ByteOrder;
+import java.nio.file.Path;
+
+import org.jlab.clara.engine.EngineDataType;
+import org.jlab.clara.std.services.AbstractEventReaderService;
+import org.jlab.clara.std.services.EventReaderException;
+import org.jlab.coda.jevio.EvioException;
+import org.jlab.io.evio.EvioSource;
+import org.json.JSONObject;
+
+/**
+ * Converter service that converts EvIO persistent data to EvIO transient data
+ * (i.e. Reads EvIO events from an input file)
+ */
+public class EvioToEvioSource extends AbstractEventReaderService<EvioSource> {
+
+    protected EvioSource createReader(Path file, JSONObject opts) throws EventReaderException {
+        return new EvioSource(file.toString());
+    }
+
+    @Override
+    protected void closeReader() {
+        reader.close();
+    }
+
+    @Override
+    public int readEventCount() throws EventReaderException {
+        return reader.getEventCount();
+    }
+
+    @Override
+    public ByteOrder readByteOrder() throws EventReaderException {
+        return reader.getFileByteOrder();
+    }
+
+    @Override
+    public Object readEvent(int eventNumber) throws EventReaderException {
+        try {
+            return reader.getEventBuffer(++eventNumber, true);
+        } catch (EvioException e) {
+            throw new EventReaderException(e);
+        }
+    }
+
+    @Override
+    protected EngineDataType getDataType() {
+        return Clas12Types.EVIO;
+    }
+}
