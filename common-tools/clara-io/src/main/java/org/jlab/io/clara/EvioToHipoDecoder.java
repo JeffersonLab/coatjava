cc783baa81017a6e6ff619dbfd67c28a5c45b818
Nathan Baltzell <baltzell@jlab.org>
2025-05-08 19:59:24 -0400

    do decoding

--- common-tools/clara-io/src/main/java/org/jlab/io/clara/EvioToHipoDecoder.java
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/EvioToHipoDecoder.java
@@ -7,8 +7,10 @@ import org.jlab.clara.engine.EngineDataType;
 import org.jlab.clara.std.services.AbstractEventReaderService;
 import org.jlab.clara.std.services.EventReaderException;
 import org.jlab.coda.jevio.EvioException;
+import org.jlab.detector.decode.CLASDecoder4;
 import org.jlab.io.evio.EvioDataEvent;
 import org.jlab.io.evio.EvioSource;
+import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
 import org.json.JSONObject;
 
@@ -17,9 +19,15 @@ import org.json.JSONObject;
  * @author baltzell
  */
 public class EvioToHipoDecoder extends AbstractEventReaderService<EvioSource> {
-    
+
+    CLASDecoder4 decoder;
     private ByteOrder byteOrder;
     private long maxEvents;
+    private int runNumber=-1;
+    private float torus=1;
+    private float solenoid=1;
+    Bank rawScaler;
+    Bank rawRunConf;
 
     @Override
     protected EvioSource createReader(Path file, JSONObject opts) throws EventReaderException {
@@ -27,6 +35,9 @@ public class EvioToHipoDecoder extends AbstractEventReaderService<EvioSource> {
         s.open(file.toString());
         byteOrder = s.getFileByteOrder();
         maxEvents = s.getEventCount();
+        decoder = new CLASDecoder4();
+        rawScaler   = new Bank(decoder.getSchemaFactory().getSchema("RAW::scaler"));
+        rawRunConf  = new Bank(decoder.getSchemaFactory().getSchema("RUN::config"));
         return s;
     }
 
@@ -54,8 +65,12 @@ public class EvioToHipoDecoder extends AbstractEventReaderService<EvioSource> {
         if (eventNumber >= maxEvents) return null;
         try {
             ByteBuffer bb = reader.getEventBuffer(++eventNumber, true);
-			EvioDataEvent event = new EvioDataEvent(bb.array(), byteOrder, reader.getDictionary());
-            return event;
+			EvioDataEvent evio = new EvioDataEvent(bb.array(), byteOrder, reader.getDictionary());
+            Event hipo = decoder.getDecodedEvent(evio, runNumber, eventNumber, torus, solenoid);
+            for (Bank b : decoder.createReconScalerBanks(hipo)) hipo.write(b);
+            Bank epics = decoder.createEpicsBank();
+            if (epics != null) hipo.write(epics);
+            return hipo;
         } catch (EvioException e) {
             throw new EventReaderException(e);
         }
@@ -63,6 +78,6 @@ public class EvioToHipoDecoder extends AbstractEventReaderService<EvioSource> {
 
     @Override
     protected EngineDataType getDataType() {
-        return Clas12Types.EVIO;
+        return Clas12Types.HIPO;
     }
 }
