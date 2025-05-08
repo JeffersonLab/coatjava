8b802754c406d69737a36e66720f7d1244136e0d
Nathan Baltzell <baltzell@jlab.org>
2025-05-08 18:48:29 -0400

    wrapper methods

--- common-tools/clas-io/src/main/java/org/jlab/io/evio/EvioSource.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/evio/EvioSource.java
@@ -25,6 +25,19 @@ public class EvioSource implements DataSource {
 	private EvioDataDictionary dictionary = new EvioDataDictionary();
 	private String dictionaryPath = "some";
 
+    public void close() {
+        evioReader.close();
+    }
+    public int getEventCount() {
+        return evioReader.getEventCount();
+    }
+    public ByteOrder getFileByteOrder() {
+        return evioReader.getFileByteOrder();
+    }
+    public Object getEventBuffer(int eventNumber, boolean asdf) throws EvioException {
+        return evioReader.getEventBuffer(eventNumber, asdf);
+    }
+    
 	public EvioSource() {
 
 		String CLAS12DIR = System.getenv("CLAS12DIR");
@@ -120,11 +133,6 @@ public class EvioSource implements DataSource {
 		} catch (EvioException ex) {
 			Logger.getLogger(EvioSource.class.getName()).log(Level.SEVERE, null, ex);
 		}
-	}
-
-	public void close() {
-		// TODO Auto-generated method stub
-
 	}
 
 	public int getSize() {
