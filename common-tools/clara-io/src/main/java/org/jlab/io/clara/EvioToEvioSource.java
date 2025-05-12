5951e68af8f8b29f9989fcc74106aae759f952b4
Nathan Baltzell <baltzell@gmx.com>
2025-05-11 20:10:00 -0400

    Io dev (#618)
    * add i/o dependency
    
    * rename
    
    * wrapper methods
    
    * add EVIO CLARA source
    
    * restore annotation
    
    * add access
    
    * copy some optimizations
    
    * prep decoder io service
    
    * resuse decoder parts
    
    * add accessor
    
    * do decoding
    
    * tabs->spaces
    
    * cleanup
    
    * add example
    
    * cleanup
    
    * cleanup
    
    * rename
    
    * cleanup, rename
    
    * add metadata
    
    * update docs
    
    * open acces
    
    * add a tagging hipo writer, like decoding
    
    * copy RUN::config too
    
    * bugfix
    
    * cleanup
    
    * add more "decoding"
    
    * add flag
    
    * move garbage collector to reader where it belongs
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * gitignore clara-home
    
    * install clara too
    
    * gitignore tmp
    
    * hardcode endianness
    
    * set more java options
    
    * add access
    
    * add config support for ccdb timestamp/variation
    
    * cleanup
    
    * cleanup
    
    * rename and use tag writer
    
    * bugfix
    
    * metadata bugfix
    
    * bugfix
    
    * test disabling macos
    
    * disable clara install
    
    * reenable macos

---
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/EvioToEvioSource.java
@@ -0,0 +1,64 @@
+package org.jlab.io.clara;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Path;
+
+import org.jlab.clara.engine.EngineDataType;
+import org.jlab.clara.std.services.AbstractEventReaderService;
+import org.jlab.clara.std.services.EventReaderException;
+import org.jlab.coda.jevio.EvioException;
+import org.jlab.io.evio.EvioDataEvent;
+import org.jlab.io.evio.EvioSource;
+import org.json.JSONObject;
+
+/**
+ * Converter service that converts EvIO persistent data to EvIO transient data
+ * (i.e. Reads EvIO events from an input file)
+ */
+public class EvioToEvioSource extends AbstractEventReaderService<EvioSource> {
+
+    private ByteOrder byteOrder;
+    private long maxEvents;
+
+    @Override
+    protected EvioSource createReader(Path file, JSONObject opts) throws EventReaderException {
+        EvioSource s = new EvioSource();
+        s.open(file.toString());
+        byteOrder = s.getFileByteOrder();
+        maxEvents = s.getEventCount();
+        return s;
+    }
+
+    @Override
+    protected void closeReader() {
+        reader.close();
+    }
+
+    @Override
+    public int readEventCount() throws EventReaderException {
+        return reader.getEventCount();
+    }
+
+    @Override
+    public ByteOrder readByteOrder() throws EventReaderException {
+        return reader.getFileByteOrder();
+    }
+
+    @Override
+    public Object readEvent(int eventNumber) throws EventReaderException {
+        if (eventNumber >= maxEvents) return null;
+        try {
+            ByteBuffer bb = reader.getEventBuffer(++eventNumber, true);
+            EvioDataEvent event = new EvioDataEvent(bb.array(), byteOrder, reader.getDictionary());
+            return event;
+        } catch (EvioException e) {
+            throw new EventReaderException(e);
+        }
+    }
+
+    @Override
+    protected EngineDataType getDataType() {
+        return Clas12Types.EVIO;
+    }
+}
