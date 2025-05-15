cacc59d5c885afb5c08c20622c31a38e01ca44ae
Nathan Baltzell <baltzell@jlab.org>
2025-05-15 19:47:38 -0400

    get delay from CCDB, reuse writer logic

--- common-tools/clara-io/src/main/java/org/jlab/io/clara/HipoToHipoPostWriter.java
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/HipoToHipoPostWriter.java
@@ -6,7 +6,6 @@ import org.jlab.detector.helicity.HelicitySequence;
 import org.jlab.detector.helicity.HelicitySequenceDelayed;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.io.HipoReader;
-import org.jlab.jnp.hipo4.io.HipoWriterSorted;
 
 /**
  * Adds "postprocessing", i.e., copying tag-1 data to physics events. 
@@ -16,31 +15,32 @@ import org.jlab.jnp.hipo4.io.HipoWriterSorted;
 public class HipoToHipoPostWriter extends HipoToHipoTagWriter {
  
     protected void update() {
-        HelicitySequenceDelayed h = new HelicitySequenceDelayed(8);
-        h.addStream(helicityReadings);
-        Processor p = new Processor(fullSchema, h, daqScalers);
+        int d = conman.getConstants(runNumber, "/runcontrol/helicity").getIntValue("delay",0,0,0);
+        HelicitySequenceDelayed h = new HelicitySequenceDelayed(d);
+        h.addStream(helicities);
+        Processor p = new Processor(fullSchema, h, scalers);
         HipoReader r = new HipoReader();
-        HipoWriterSorted w = new HipoWriterSorted();
-        w.open("pp_"+filename);
         r.open(filename);
         Event e = new Event();
+        writer.open("pp_"+filename);
         while (r.hasNext()) {
             r.nextEvent(e);
             p.processEvent(e);
-            w.addEvent(e);
+            HipoToHipoWriter.writeEvent(writer, e, schemaBankList);
         }
-        w.close();
+        writer.close();
         new File(filename).delete();
         new File("pp_"+filename).renameTo(new File(filename));
+        
     }
     
     @Override
     protected void closeWriter() {
-        HelicitySequence.writeFlips(fullSchema, writer, helicityReadings);
+        HelicitySequence.writeFlips(fullSchema, writer, helicities);
         super.closeRawWriter();
         update();
-        helicityReadings.clear();
-        daqScalers.clear();
+        helicities.clear();
+        scalers.clear();
     }
 
 }
