995883cccf7509ef6d0efa751167ad551f9d0388
Raffaella De Vita <devita@ge.infn.it>
2025-07-14 18:48:44 -0400

    initializing the urwell service for ddvcs

--- reconstruction/urwell/src/main/java/org/jlab/service/urwell/URWellEngine.java
+++ reconstruction/urwell/src/main/java/org/jlab/service/urwell/URWellEngine.java
@@ -41,7 +41,7 @@ public class URWellEngine extends ReconstructionEngine {
         // init ConstantsManager to read constants from CCDB
         String variationName = Optional.ofNullable(this.getEngineConfigString("variation")).orElse("default");
         DatabaseConstantProvider cp = new DatabaseConstantProvider(11, variationName);
-        factory.init(cp, false, URWellConstants.NREGION);
+        factory.init(cp, 2, URWellConstants.NREGION);
         // register output banks for drop option        
         this.registerOutputBank("URWELL::hits");
         this.registerOutputBank("URWELL::clusters");
