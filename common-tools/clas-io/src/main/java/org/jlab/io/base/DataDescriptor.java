1119c360abbfc7f595362352b7483b36ebccc837
Nathan Baltzell <baltzell@jlab.org>
2025-07-16 14:41:31 -0400

    cleanup, tabs/spaces, formatting, ide warnings

--- common-tools/clas-io/src/main/java/org/jlab/io/base/DataDescriptor.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/base/DataDescriptor.java
@@ -5,15 +5,15 @@ package org.jlab.io.base;
  * It consists of a list of Entries by name and integer ID.
  */
 public interface DataDescriptor {
-	void init(String s);
-	String[] getEntryList();
-	String getName();
-        String getXML();
-        boolean  hasEntry(String entry);
-        boolean  hasEntries(String... entries);
-	int getProperty(String property_name, String entry_name);
-	int getProperty(String property_name);
-        void    setPropertyString(String name, String value);
-        String  getPropertyString(String property_name);
-        void show();
+    void init(String s);
+    String[] getEntryList();
+    String getName();
+    String getXML();
+    boolean hasEntry(String entry);
+    boolean hasEntries(String... entries);
+    int getProperty(String property_name, String entry_name);
+    int getProperty(String property_name);
+    void setPropertyString(String name, String value);
+    String getPropertyString(String property_name);
+    void show();
 }
