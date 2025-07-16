e6795621c570b993a590e2093fb425efe8221f9d
Nathan Baltzell <baltzell@jlab.org>
2025-07-16 15:48:08 -0400

    cleanup, formatting, tabs/spaces, ide warnings

--- common-tools/clas-io/src/main/java/org/jlab/io/ui/ConnectionDialogHipo.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/ui/ConnectionDialogHipo.java
@@ -7,7 +7,6 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
@@ -25,23 +24,18 @@ public class ConnectionDialogHipo extends BasicDialog {
     public static final int   RING_TYPE_ET = 5;
     public static final int RING_TYPE_HIPO = 6;
     
-    private int  connectionType = ConnectionDialog.RING_TYPE_ET;
-    
     private JRadioButton _directConnect;
     private JRadioButton _connectToDAQ;
     
     private static String[] hostNames = new String[]{"clondaq2","clondaq3","clondaq4","clondaq5","clondaq6","clondaq7"};
     private static String[]    hostIP = new String[]{"129.57.167.109","129.57.167.226","129.57.167.227","129.57.167.41","129.57.167.60","129.57.167.20"};
     
-    Map<String,String>  connectionHosts = new LinkedHashMap<String,String>();
+    Map<String,String>  connectionHosts = new LinkedHashMap<>();
     
     private static String[] closeoutButtons = {"Connect", "Cancel"};
     
     private JTextField _ipField;
     private JTextField _fileName;
-    private JComboBox  _comboHosts;
-    private JComboBox  _comboEtFiles;
-    private JComboBox  _files;
     
     private int _reason = DialogUtilities.CANCEL_RESPONSE;
     
@@ -50,7 +44,6 @@ public class ConnectionDialogHipo extends BasicDialog {
      */
     public ConnectionDialogHipo() {
         super("Connection.....", true, closeoutButtons);
-        int nhosts = hostNames.length;
         for(int i = 0; i < hostNames.length; i++){
             this.connectionHosts.put(hostNames[i],hostIP[i]);
         }
@@ -77,9 +70,7 @@ public class ConnectionDialogHipo extends BasicDialog {
         panel.add(Box.createVerticalStrut(6));
         panel.add(_connectToDAQ);
         
-        Border emptyBorder = BorderFactory
-                .createEtchedBorder();//4, 4, 4, 4);
-        
+        Border emptyBorder = BorderFactory.createEtchedBorder();
         panel.setBorder(BorderFactory.createTitledBorder(emptyBorder, "Connect to Host"));
         return panel;
     }
