81ee1a1e743825169374c4ef9fd25e285930bdb1
Nathan Baltzell <baltzell@gmx.com>
2025-07-18 22:40:54 -0400

    cleanup

--- common-tools/clas-detector/src/main/java/org/jlab/detector/decode/DetectorEventDecoder.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/decode/DetectorEventDecoder.java
@@ -146,10 +146,8 @@ public class DetectorEventDecoder {
             int slot     = data.getDescriptor().getSlot();
             int channel  = data.getDescriptor().getChannel();
             for(String table : keysFitter){
-                //custom MM fitter
             	if( ( (table.equals("BMT"))&&(data.getDescriptor().getType().getName().equals("BMT")) )
                  || ( (table.equals("FMT"))&&(data.getDescriptor().getType().getName().equals("FMT")) )
-                 //|| ( (table.equals("AHDC"))&&(data.getDescriptor().getType().getName().equals("AHDC")) )
                  || ( (table.equals("FTTRK"))&&(data.getDescriptor().getType().getName().equals("FTTRK")) ) ){
                     IndexedTable daq = fitterManager.getConstants(runNumber, table);
                     short adcOffset = (short) daq.getDoubleValue("adc_offset", 0, 0, 0);
