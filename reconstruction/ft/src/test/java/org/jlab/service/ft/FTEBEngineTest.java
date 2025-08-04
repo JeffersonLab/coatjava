211cfa5fd4aafa9c107600669a3d7e3f2ebbf14a
Nathan Baltzell <baltzell@jlab.org>
2025-08-04 12:31:46 -0400

    remove unused imports

--- reconstruction/ft/src/test/java/org/jlab/service/ft/FTEBEngineTest.java
+++ reconstruction/ft/src/test/java/org/jlab/service/ft/FTEBEngineTest.java
@@ -4,40 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.swing.JFrame;
-import org.jlab.clas.detector.DetectorData;
-import org.jlab.clas.detector.DetectorEvent;
-
 import org.jlab.clas.reco.ReconstructionEngine;
-import org.jlab.detector.base.DetectorLayer;
-import org.jlab.geom.prim.Line3D;
 import org.jlab.geom.prim.Point3D;
-import org.jlab.geom.prim.Vector3D;
 import org.jlab.groot.data.H1F;
 import org.jlab.groot.data.H2F;
-import org.jlab.groot.data.DataLine;
-import org.jlab.groot.ui.LatexText;
-import org.jlab.groot.math.F1D;
-import org.jlab.groot.fitter.DataFitter;
-import org.jlab.groot.graphics.EmbeddedCanvas;
-import org.jlab.groot.data.GraphErrors;
-import org.jlab.clas.pdg.PhysicsConstants;
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
 import org.jlab.io.evio.EvioDataBank;
 import org.jlab.io.evio.EvioDataEvent;
-import org.jlab.io.hipo.HipoDataSource;
-import org.jlab.rec.ft.cal.FTCALConstantsLoader;
-import org.jlab.rec.ft.cal.FTCALEngine;
-import org.jlab.rec.ft.hodo.FTHODOEngine;
-import org.jlab.rec.ft.trk.FTTRKEngine;
-import org.jlab.rec.ft.trk.FTTRKConstantsLoader;
-import org.jlab.rec.ft.trk.FTTRKReconstruction;
 import org.jlab.rec.ft.FTEventBuilder;
 import org.jlab.rec.ft.FTParticle;
 import org.jlab.rec.ft.FTResponse;
-import org.jlab.rec.ft.FTEBEngine;
-import org.jlab.rec.ft.FTConstants;
 
 public class FTEBEngineTest extends ReconstructionEngine {
 
