626289576a377bfc0284306e0bc7902504226de3
Nathan Baltzell <baltzell@jlab.org>
2025-09-05 19:00:14 -0400

    add denoising engine

---
+++ reconstruction/dn/src/main/java/org/jlab/service/dn/DenoiseEngine.java
@@ -0,0 +1,113 @@
+package org.jlab.service.dn;
+
+import java.nio.file.Paths;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.Shape;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.training.util.ProgressBar;
+import ai.djl.translate.Translator;
+import ai.djl.translate.TranslatorContext;
+import ai.djl.inference.Predictor;
+import ai.djl.translate.Batchifier;
+
+import org.jlab.clas.reco.ReconstructionEngine;
+import org.jlab.io.base.DataEvent;
+import org.jlab.utils.system.ClasUtilsFile;
+
+public class DenoiseEngine extends ReconstructionEngine {
+    
+    Criteria<float[][],float[][]> criteria;
+
+    public DenoiseEngine() {
+        super("DenoiseEngine","lleztlab","1.0");
+    }
+
+    @Override
+    public boolean init() {
+        criteria = Criteria.builder()
+            .setTypes(float[][].class, float[][].class)
+            .optModelPath(Paths.get(ClasUtilsFile.getResourceDir("etc/nnet/dn/cnn_autoenc_0f_112.pt")))
+            .optEngine("PyTorch")
+            .optTranslator(DenoiseEngine.getTranslator())
+            .optProgress(new ProgressBar())
+            .build();
+        return true;
+    }
+
+    @Override
+    public boolean processDataEvent(DataEvent event) {
+        try {
+            ZooModel<float[][], float[][]> model = criteria.loadModel();
+            Predictor<float[][], float[][]> predictor = model.newPredictor();
+            float[][] input = DenoiseEngine.getAlmostStraightSlightlyBendingTrack();
+            float[][] output = predictor.predict(input);
+            show(output);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+    public static void show(float[][] data) {
+        System.out.println("Output shape: [" + data.length + "," + data[0].length + "]");
+        System.out.println("Output values:");
+        for (int i = 0; i < 36; i++) {
+            for (int j = 0; j < 112; j++)
+                System.out.printf("%.3f ", data[i][j]);
+            System.out.println();
+        }
+    }
+
+    public static float[][] getAlmostStraightSlightlyBendingTrack() {
+        float[][] ret = new float[36][112];
+        for (int y = 0; y < 36; y++) {
+            int x = 50 + (y / 10);
+            ret[y][x] = 1.0f;
+        }
+        return ret;
+    }
+
+    public static Translator<float[][],float[][]> getTranslator() {
+        return new Translator<float[][],float[][]>() {
+            @Override
+            public NDList processInput(TranslatorContext ctx, float[][] input) throws Exception {
+                NDManager manager = ctx.getNDManager();
+                int height = input.length;
+                int width = input[0].length;
+                float[] flat = new float[height * width];
+                for (int i = 0; i < height; i++) {
+                    System.arraycopy(input[i], 0, flat, i * width, width);
+                }
+                NDArray x = manager.create(flat, new Shape(height, width));
+                // Add batch and channel dims -> [1,1,36,112]
+                x = x.expandDims(0).expandDims(0);
+                return new NDList(x);
+            }
+            @Override
+            public float[][] processOutput(TranslatorContext ctx, NDList list) throws Exception {
+                NDArray result = list.get(0);
+                // Remove batch and channel dims -> [36,112]
+                result = result.squeeze();
+                // Convert to 1D float array
+                float[] flat = result.toFloatArray();
+                // Reshape manually into 2D array
+                long[] shape = result.getShape().getShape();
+                int height = (int) shape[0];
+                int width = (int) shape[1];
+                float[][] output2d = new float[height][width];
+                for (int i = 0; i < height; i++) {
+                    System.arraycopy(flat, i * width, output2d[i], 0, width);
+                }
+                return output2d;
+            }
+            @Override
+            public Batchifier getBatchifier() {
+                return null; // no batching
+            }
+        };
+    }
+}
