e666342a98cb243584475b00b27b2679d76c4586
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-09-22 15:04:33 +0000

    fix: formatting

--- common-tools/clas-io/src/main/java/org/jlab/io/util/StdOutErrLogHandler.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/util/StdOutErrLogHandler.java
@@ -5,7 +5,20 @@ import java.util.logging.*;
 public class StdOutErrLogHandler {
     public static void configureLogger(Logger logger) {
         logger.setUseParentHandlers(false);
-        Handler infoHandler = new StreamHandler(System.out, new SimpleFormatter()) {
+        SimpleFormatter formatter = new SimpleFormatter() {
+            private static final String format = "%5$s%6$s%n";
+            @Override
+            public synchronized String format(LogRecord lr) {
+                return String.format(format,
+                        lr.getSourceClassName(),
+                        lr.getSourceMethodName(),
+                        lr.getLoggerName(),
+                        lr.getLevel().getLocalizedName(),
+                        lr.getMessage(),
+                        lr.getThrown() == null ? "" : lr.getThrown());
+            }
+        };
+        Handler infoHandler = new StreamHandler(System.out, formatter) {
             @Override
             public synchronized void publish(LogRecord record) {
                 if (record.getLevel().intValue() <= Level.INFO.intValue()) {
@@ -14,7 +27,7 @@ public class StdOutErrLogHandler {
                 }
             }
         };
-        Handler errorHandler = new StreamHandler(System.err, new SimpleFormatter()) {
+        Handler errorHandler = new StreamHandler(System.err, formatter) {
             @Override
             public synchronized void publish(LogRecord record) {
                 if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
