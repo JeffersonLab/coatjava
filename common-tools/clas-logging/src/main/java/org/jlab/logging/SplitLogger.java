41fb6bd6d4264acd0e44a9b83c7a805c2c503dca
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-09-22 16:36:09 -0400

    fix: propagate level

--- common-tools/clas-logging/src/main/java/org/jlab/logging/SplitLogger.java
+++ common-tools/clas-logging/src/main/java/org/jlab/logging/SplitLogger.java
@@ -3,15 +3,34 @@ package org.jlab.logging;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Helper methods to create a `Logger` that sends errors to `stderr` and everything else to `stdout`
+ * @author dilks
+ */
 public class SplitLogger {
 
   /**
-   * Configure a logger so INFO and below go to stdout, WARNING and above to stderr, with custom formatting.
+   * @return a new `SplitLogger` instance
+   * @param the name of the logger
+   */
+  public static Logger create(String name) {
+    Logger logger = Logger.getLogger(name);
+    configureLogger(logger);
+    return logger;
+  }
+
+  /**
+   * Configure a logger such that errors go to `stderr` and everything else to `stdout`
+   * @param logger the `Logger` instance to configure
    */
   public static void configureLogger(Logger logger) {
+
+    // clear handlers
     logger.setUseParentHandlers(false);
+
+    // log message formatting
     java.util.logging.Formatter formatter = new java.util.logging.SimpleFormatter() {
-      private static final String format = "SPLIT LOGGER: %5$s%6$s%n";
+      private static final String format = "%5$s%6$s%n";
       @Override
       public synchronized String format(java.util.logging.LogRecord lr) {
         return String.format(format,
@@ -23,37 +42,43 @@ public class SplitLogger {
             lr.getThrown() == null ? "" : lr.getThrown());
       }
     };
+
+    // stdout handler
     java.util.logging.Handler infoHandler = new java.util.logging.StreamHandler(System.out, formatter) {
       @Override
       public synchronized void publish(java.util.logging.LogRecord record) {
-        if (isLoggable(record) && record.getLevel().intValue() <= Level.INFO.intValue()) {
+        if(isLoggable(record) && record.getLevel().intValue() <= Level.INFO.intValue()) {
           super.publish(record);
           flush();
         }
       }
     };
+
+    // stderr handler
     java.util.logging.Handler errorHandler = new java.util.logging.StreamHandler(System.err, formatter) {
       @Override
       public synchronized void publish(java.util.logging.LogRecord record) {
-        if (isLoggable(record) && record.getLevel().intValue() >= Level.WARNING.intValue()) {
+        if(isLoggable(record) && record.getLevel().intValue() >= Level.WARNING.intValue()) {
           super.publish(record);
           flush();
         }
       }
     };
+
+    // tell the handlers which log level
+    Level thisLevel = logger.getLevel();
+    if(thisLevel==null) { // caller did not set log level, use parent
+      thisLevel = logger.getParent().getLevel();
+      if(thisLevel==null) { // should never happen, but just in case
+        thisLevel = Level.INFO;
+        System.err.println("WARNING: trouble setting level of logger '" + logger.getName() + "'; defaulting to level '" + thisLevel + "'");
+      }
+      logger.setLevel(thisLevel);
+    }
+    infoHandler.setLevel(thisLevel);
+    errorHandler.setLevel(thisLevel);
     logger.addHandler(infoHandler);
     logger.addHandler(errorHandler);
-  }
-
-  /**
-   * create a split logger
-   */
-  public static Logger create(String name) {
-    Logger logger = Logger.getLogger(name);
-    System.out.println("CREATED LOGGER " + logger.getName() + " with level " + logger.getLevel()); // FIXME: remove
-    configureLogger(logger);
-    System.out.println("CONFIG  LOGGER " + logger.getName() + " with level " + logger.getLevel()); // FIXME: remove
-    return logger;
   }
 
 }
--- potato.sh
+++ potato.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-./build-coatjava.sh -T8
-java -Dorg.jlab.logging.TestSplitLogger.level=FINEST -Xmx1536m -Xms1024m -XX:+UseSerialGC -cp '/home/dilks/j/coatjava/coatjava/lib/clas/*:/home/dilks/j/coatjava/coatjava/lib/services/*:/home/dilks/j/coatjava/coatjava/lib/utils/*' org.jlab.logging.TestSplitLogger
+# ./build-coatjava.sh -T8
+java \
+  -Xmx1536m -Xms1024m -XX:+UseSerialGC \
+  -Djava.util.logging.config.file=tomato.properties \
+  -cp '/home/dilks/j/coatjava/coatjava/lib/clas/*:/home/dilks/j/coatjava/coatjava/lib/services/*:/home/dilks/j/coatjava/coatjava/lib/utils/*' \
+  org.jlab.logging.TestSplitLogger
+
+  # -Djava.util.logging.level=FINEST \
+  # -Dorg.jlab.logging.TestSplitLogger.level=FINEST \
