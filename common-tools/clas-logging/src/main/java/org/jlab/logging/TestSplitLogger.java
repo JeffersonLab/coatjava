42570a38b1a26f5f5edb19f51c975ea79adfc2fc
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-09-22 16:36:09 -0400

    fix: propagate level

--- common-tools/clas-logging/src/main/java/org/jlab/logging/TestSplitLogger.java
+++ common-tools/clas-logging/src/main/java/org/jlab/logging/TestSplitLogger.java
@@ -1,30 +1,44 @@
 package org.jlab.logging;
 
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class TestSplitLogger {
 
-  protected static final Logger LOGGER = SplitLogger.create(TestSplitLogger.class.getName());
+  protected Logger LOGGER = SplitLogger.create(TestSplitLogger.class.getName());
+  // static final Logger LOGGER = Logger.getLogger(TestSplitLogger.class.getName());
 
-  public TestSplitLogger()
-  {
+  public TestSplitLogger() {
+    System.out.println("Created 'TestSplitLogger' instance");
+    System.out.println("Log level = " + LOGGER.getLevel());
   }
 
-  public void test()
-  {
-    LOGGER.severe(  "TEST MESSAGE SEVERE  - should go to stderr" );
-    LOGGER.warning( "TEST MESSAGE WARNING - should go to stderr" );
-    LOGGER.info(    "TEST MESSAGE INFO    - should go to stdout" );
-    LOGGER.config(  "TEST MESSAGE CONFIG  - should go to stdout" );
-    LOGGER.fine(    "TEST MESSAGE FINE    - should go to stdout" );
-    LOGGER.finer(   "TEST MESSAGE FINER   - should go to stdout" );
-    LOGGER.finest(  "TEST MESSAGE FINEST  - should go to stdout" );
+  public void test1() {
+    System.out.println("=== TEST 1 ===");
+    LOGGER.severe("SEVERE MESSAGE - should go to stderr" );
+    LOGGER.warning("WARNING MESSAGE - should go to stderr" );
+    LOGGER.info("INFO MESSAGE - should go to stdout" );
+    LOGGER.config("CONFIG MESSAGE - should go to stdout" );
+    LOGGER.fine("FINE MESSAGE - should go to stdout" );
+    LOGGER.finer("FINER MESSAGE - should go to stdout" );
+    LOGGER.finest("FINEST MESSAGE - should go to stdout" );
   }
 
-  public static void main(String[] args)
-  {
-    var t = new TestSplitLogger();
-    t.test();
+  public void test2() {
+    System.out.println("=== TEST 2 ===");
+    LOGGER.log(Level.SEVERE, "{0} MESSAGE - should go to stderr", Level.SEVERE);
+    LOGGER.log(Level.WARNING, "{0} MESSAGE - should go to stderr", Level.WARNING);
+    LOGGER.log(Level.INFO, "{0} MESSAGE - should go to stdout", Level.INFO);
+    LOGGER.log(Level.CONFIG, "{0} MESSAGE - should go to stdout", Level.CONFIG);
+    LOGGER.log(Level.FINE, "{0} MESSAGE - should go to stdout", Level.FINE);
+    LOGGER.log(Level.FINER, "{0} MESSAGE - should go to stdout", Level.FINER);
+    LOGGER.log(Level.FINEST, "{0} MESSAGE - should go to stdout", Level.FINEST);
+  }
+
+  public static void main(String[] args) {
+    var potato = new TestSplitLogger();
+    potato.test1();
+    potato.test2();
   }
 
 }
