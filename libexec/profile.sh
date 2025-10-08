b8fd23d84c5782c237700eafb298997f9adfebb8
Nathan Baltzell <baltzell@gmx.com>
2025-10-08 16:06:22 -0400

    feat:  gitlab updates (#864)
    * gitlab ci prep
    
    * add missing libexec placeholder
    
    * Update .gitlab-ci.yml
    
    Co-authored-by: Christopher Dilks <c-dilks@users.noreply.github.com>
    
    * Update .gitlab-ci.yml
    
    Co-authored-by: Christopher Dilks <c-dilks@users.noreply.github.com>
    
    * Update .gitlab-ci.yml
    
    Co-authored-by: Christopher Dilks <c-dilks@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Christopher Dilks <c-dilks@users.noreply.github.com>

---
+++ libexec/profile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo PROFILE!!!
+
+#asprof -e cpu -d 120 \
+#  -o tree -f results/$1_tree_cpu.html \
+#  $(pidof java)
+
+#asprof -e nativemem -d 120 \
+#  -o tree -f results/$1_tree_nativemem.html \
+#  $(pidof java)
+
+#asprof -e nativemem -d 120 \
+#  -f results/$1_mem.jfr \
+#  $(pidof java)
+
