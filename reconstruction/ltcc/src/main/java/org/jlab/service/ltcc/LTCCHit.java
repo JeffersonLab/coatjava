c0ffbf7c859b3c03e8650def90968845f0d762f0
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-10-16 17:00:43 -0400

    fix: undeclared dependency

--- reconstruction/ltcc/src/main/java/org/jlab/service/ltcc/LTCCHit.java
+++ reconstruction/ltcc/src/main/java/org/jlab/service/ltcc/LTCCHit.java
@@ -7,7 +7,7 @@ import org.jlab.utils.groups.IndexedTable;
 
 import java.util.LinkedList;
 import java.util.List;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 import org.jlab.detector.banks.RawDataBank;
 import org.jlab.geom.prim.Vector3D;
 
