16a18e2cd59506150f7d836054cc178ea30679ae
Nathan Baltzell <baltzell@jlab.org>
2025-11-03 17:35:38 -0500

    add Benchmark option to ProgressPrintout

--- common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/ProgressPrintout.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/ProgressPrintout.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jlab.utils.benchmark;
 
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
@@ -17,15 +11,14 @@ import java.util.logging.Logger;
  */
 public class ProgressPrintout {
     
-    private TreeMap<String,Object> items   = new TreeMap<String,Object>();
-    private TreeMap<String,Object> itemMax = new TreeMap<String,Object>();
+    private TreeMap<String,Object> items   = new TreeMap<>();
     
-    private Long                   previousPrintoutTime = (long) 0;
-    private Long                   startPrintoutTime    = (long) 0;
-    
-    private double   printoutIntervalSeconds = 10.0;
-    private String   printoutLeadingString   = ">>>>> progress : ";
-    private Integer  numberOfCalls           = 0;
+    private Long previousPrintoutTime = (long) 0;
+    private Long startPrintoutTime    = (long) 0;
+    private double printoutIntervalSeconds = 10.0;
+    private String printoutLeadingString = ">>>>> progress : ";
+    private Integer numberOfCalls = 0;
+    private Benchmark benchmark = null;
     
     public  ProgressPrintout(){
         this.previousPrintoutTime = System.currentTimeMillis();
@@ -41,7 +34,11 @@ public class ProgressPrintout {
     public void setInterval(double interval){
         this.printoutIntervalSeconds = interval;
     }
-    
+
+    public void addBenchmarks() {
+        benchmark = Benchmark.getInstance();
+    }
+
     public String getUpdateString(){
         double totalElapsedTime = (this.previousPrintoutTime-this.startPrintoutTime)*1e-3;
         StringBuilder str = new StringBuilder();
@@ -56,6 +53,7 @@ public class ProgressPrintout {
     }
     
     public void showStatus(){
+        if (benchmark != null) System.out.println(benchmark);
         System.out.println("\n\n");
         System.out.println(this.getUpdateString());
         System.out.println("\n\n");        
@@ -65,10 +63,8 @@ public class ProgressPrintout {
         this.numberOfCalls++;
         Long currentTime   = System.currentTimeMillis();
         Double elapsedTime = (currentTime - this.previousPrintoutTime)*1e-3;
-        //System.out.println("elapsed = " + elapsedTime);
         if(elapsedTime>=this.printoutIntervalSeconds){
             this.previousPrintoutTime = System.currentTimeMillis();
-            //System.out.println(" passed time ");
             System.out.println(this.getUpdateString());
         }
     }
@@ -103,7 +99,6 @@ public class ProgressPrintout {
             } catch (InterruptedException ex) {
                 Logger.getLogger(ProgressPrintout.class.getName()).log(Level.SEVERE, null, ex);
             }
-            //System.out.println("cycle " + loop);
             progress.updateStatus();
         }
     }
