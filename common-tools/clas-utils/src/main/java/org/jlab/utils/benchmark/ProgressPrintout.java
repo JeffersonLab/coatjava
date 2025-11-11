4f8c12071024baacf768fb3e307ff7cd885a0667
Nathan Baltzell <baltzell@jlab.org>
2025-11-11 16:38:33 -0500

    cleanup

--- common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/ProgressPrintout.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/ProgressPrintout.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jlab.utils.benchmark;
 
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
@@ -17,12 +11,9 @@ import java.util.logging.Logger;
  */
 public class ProgressPrintout {
     
-    private TreeMap<String,Object> items   = new TreeMap<String,Object>();
-    private TreeMap<String,Object> itemMax = new TreeMap<String,Object>();
-    
-    private Long                   previousPrintoutTime = (long) 0;
-    private Long                   startPrintoutTime    = (long) 0;
-    
+    private TreeMap<String,Object> items   = new TreeMap<>();
+    private Long     previousPrintoutTime = (long) 0;
+    private Long     startPrintoutTime    = (long) 0;
     private double   printoutIntervalSeconds = 10.0;
     private String   printoutLeadingString   = ">>>>> progress : ";
     private Integer  numberOfCalls           = 0;
@@ -83,12 +74,12 @@ public class ProgressPrintout {
     
     public String getItemString(String itemname){
         StringBuilder str = new StringBuilder();
-        if(this.items.get(itemname) instanceof Integer){
-            str.append(String.format("  %s : %5d",itemname,(Integer)this.items.get(itemname)));
+        if(this.items.get(itemname) instanceof Integer integer){
+            str.append(String.format("  %s : %5d",itemname, integer));
         }
         
-        if(this.items.get(itemname) instanceof Double){
-            str.append(String.format("  %s : %8.3f",itemname,(Double)this.items.get(itemname)));
+        if(this.items.get(itemname) instanceof Double aDouble){
+            str.append(String.format("  %s : %8.3f",itemname, aDouble));
         }        
         return str.toString();
     }
@@ -103,7 +94,6 @@ public class ProgressPrintout {
             } catch (InterruptedException ex) {
                 Logger.getLogger(ProgressPrintout.class.getName()).log(Level.SEVERE, null, ex);
             }
-            //System.out.println("cycle " + loop);
             progress.updateStatus();
         }
     }
