477fed8acb5b578a758406529a0d2141e1a4c4b1
Nathan Baltzell <baltzell@jlab.org>
2025-12-03 19:03:46 -0500

    add convenience method

--- common-tools/clas-detector/src/main/java/org/jlab/detector/base/DetectorDescriptor.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/base/DetectorDescriptor.java
@@ -94,6 +94,14 @@ public class DetectorDescriptor implements Comparable<DetectorDescriptor> {
         this.dt_COMPONENT = comp;
     }
 
+    public final void setSectorLayerComponentOrderType(int sector, int layer, int comp, int order, int type) {
+        this.dt_SECTOR = sector;
+        this.dt_LAYER  = layer;
+        this.dt_COMPONENT = comp;
+        this.dt_ORDER = order;
+        this.detectorType = DetectorType.getType(type);
+    }
+
     public static int generateHashCode(int s, int l, int c){
         return  ((s<<24)&0xFF000000)|
                 ((l<<16)&0x00FF0000)|(c&0x0000FFFF);
