e460eaf6df7578141f20a1f30a346d66a7dea4c4
Mathieu Ouillon <67646911+mathieuouillon@users.noreply.github.com>
2025-12-11 21:12:13 +0100

    Alert track matching ai (#969)
    * Start an implementation for the track matching for ALERT
    
    * Fix the path for the ALERT AI models
    
    * ALERT: Improve the AI model for track finding
    
    * ALERT: Change name of the model class for the track finding
    
    * ALERT: Rename some variables and class (Mode to ModeTrackFinding)
    
    * ALERT: change superpreclusters to InterCluster
    
    * Move the track matching to the ALERT Engine
    
    * ALERT: change the structure of the AHDCEngine to write the interclusters and link them to their associated track.
    
    - Introduced new intercluster data structure in alert.json to store track ID and coordinates.
    - Add to InterCluster class the track ID.
    - Rename TrackConstruction to TrackCandidatesGenerator.
    - Updated Track class to manage interclusters and clusters and set track IDs.
    - Modified AHDCEngine to have similar interface for AI and conventional track finding.
    - Use the AI precluster algorithm for everthings (CV and AI)
    
    * Fix the track matching AI and the output bank
    
    * Add the model and fix the constant loader in the ATOF
    
    * Modify HipoDataSync initialization in AHDCEngine
    
    * Update model path for TrackMatchingAI to use the new path for AI models
    
    * Remove the AI networks
    
    * Update model path for ALERT model in ModelTrackFinding
    
    * Update model path for TrackMatchingAI
    
    * fix(ci): use `--lfs` with `--unittests`
    
    ---------
    
    Co-authored-by: Christopher Dilks <c-dilks@users.noreply.github.com>

--- reconstruction/alert/src/main/java/org/jlab/rec/ahdc/Banks/RecoBankWriter.java
+++ reconstruction/alert/src/main/java/org/jlab/rec/ahdc/Banks/RecoBankWriter.java
@@ -2,6 +2,7 @@ package org.jlab.rec.ahdc.Banks;
 
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
+import org.jlab.rec.ahdc.AI.InterCluster;
 import org.jlab.rec.ahdc.AI.TrackPrediction;
 import org.jlab.rec.ahdc.Cluster.Cluster;
 import org.jlab.rec.ahdc.Hit.Hit;
@@ -112,7 +113,6 @@ public class RecoBankWriter {
 			bank.setFloat("p_drift", row, (float) track.get_p_drift());
 			bank.setFloat("chi2", row, (float) track.get_chi2());
 			bank.setFloat("sum_residuals", row, (float) track.get_sum_residuals());
-
 			row++;
 		}
 
@@ -174,6 +174,23 @@ public class RecoBankWriter {
 			bank.setFloat("y5", row, (float) track.getSuperpreclusters().get(4).getY());
 
 			bank.setFloat("pred", row, track.getPrediction());
+			row++;
+		}
+
+		return bank;
+	}
+
+	public DataBank fillInterClusterBank(DataEvent event, ArrayList<InterCluster> interClusters) {
+
+		int nInterClusters = interClusters.size();
+		DataBank bank = event.createBank("AHDC::interclusters", nInterClusters);
+
+		int row = 0;
+
+		for (InterCluster interCluster : interClusters) {
+			bank.setInt("trackid", row, (int) interCluster.getTrackId());
+			bank.setFloat("x", row, (float) interCluster.getX());
+			bank.setFloat("y", row, (float) interCluster.getY());
 			row++;
 		}
 
