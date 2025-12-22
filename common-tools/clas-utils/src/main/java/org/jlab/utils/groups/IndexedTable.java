5226733aabc6d72716241c72b4c78158695b2d07
Nathan Baltzell <baltzell@jlab.org>
2025-12-22 17:28:15 -0500

    add more hash-based accessors

--- common-tools/clas-utils/src/main/java/org/jlab/utils/groups/IndexedTable.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/groups/IndexedTable.java
@@ -9,6 +9,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
@@ -120,7 +121,11 @@ public class IndexedTable extends DefaultTableModel {
             }
         }
     }
-    
+
+    public void setIntValueByHash(Integer value, int column, long hash) {
+        this.entries.getItemByHash(hash).setValue(column, value);
+    }
+
     public  void setDoubleValue(Double value, String item, int... index){
         if(this.entries.hasItem(index)==false){
             if(DEBUG_MODE>0) System.out.println( "[IndexedTable] ---> error.. entry does not exist");
@@ -164,6 +169,18 @@ public class IndexedTable extends DefaultTableModel {
             }
         }
         return 0;
+    }
+
+    public List<Number> getValuesByHash(long hash) {
+        return this.entries.getItemByHash(hash).entryValues;
+    }
+
+    public List<Integer> getIntegersByHash(long hash) {
+        return getValuesByHash(hash).stream().map(x -> x.intValue()).collect(Collectors.toList());
+    }
+
+    public List<Double> getDoublesByHash(long hash) {
+        return getValuesByHash(hash).stream().map(x -> x.doubleValue()).collect(Collectors.toList());
     }
 
     public int  getIntValue(String item, int... index){
