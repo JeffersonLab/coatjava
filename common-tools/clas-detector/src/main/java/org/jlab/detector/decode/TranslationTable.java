aedf8ce00a45acf775886a2cab45940b40c54de1
Nathan Baltzell <baltzell@jlab.org>
2025-12-22 19:32:04 -0500

    improve, test, stop recalculating hash

--- common-tools/clas-detector/src/main/java/org/jlab/detector/decode/TranslationTable.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/decode/TranslationTable.java
@@ -1,7 +1,9 @@
 package org.jlab.detector.decode;
 
 import org.jlab.detector.base.DetectorType;
+import org.jlab.detector.calib.utils.ConstantsManager;
 import org.jlab.utils.groups.IndexedTable;
+
 
 /**
  *
@@ -14,21 +16,59 @@ public class TranslationTable extends IndexedTable {
     };
 
     public void add(DetectorType dt, IndexedTable it) {
+
         for (Object key : it.getList().getMap().keySet()) {
-            int crate = IndexedTable.DEFAULT_GENERATOR.getIndex((long) key, 0);
-            int slot = IndexedTable.DEFAULT_GENERATOR.getIndex((long) key, 1);
-            int channel = IndexedTable.DEFAULT_GENERATOR.getIndex((long) key, 2);
-            long hash = IndexedTable.DEFAULT_GENERATOR.hashCode(crate, slot, channel);
+
+            // get the indices:
+            long hash = (long)key;
+            int crate = IndexedTable.DEFAULT_GENERATOR.getIndex(hash, 0);
+            int slot = IndexedTable.DEFAULT_GENERATOR.getIndex(hash, 1);
+            int channel = IndexedTable.DEFAULT_GENERATOR.getIndex(hash, 2);
+
+            if (hasEntryByHash(hash)) {
+                System.err.print("TranslationTable:  found CCDB overlap for ");
+                System.err.println(String.format("%d/%d/%d %s",crate,slot,channel,dt));
+            }
+
             // add row to the new table:
             addEntry(crate, slot, channel);
-            // add all entries to the new row:
-            int column;
-            for (column=0; column<it.getEntryMap().values().size(); column++) {
+
+            // add each column's entry to the new row:
+            for (int column=0; column<it.getEntryMap().values().size(); column++) {
                 int value = it.getIntValueByHash(column, hash);
                 setIntValueByHash(value, column, hash);
             }
-            // add the new detector type:
-            setIntValueByHash(dt.getDetectorId(), column, hash);
+
+            // add the new detector type, as the last column:
+            setIntValueByHash(dt.getDetectorId(), it.getEntryMap().values().size(), hash);
         }
+    }
+
+    
+    public static final DetectorType[] TYPES = new DetectorType[]{
+        DetectorType.FTCAL,DetectorType.FTHODO,DetectorType.FTTRK,
+        DetectorType.LTCC,DetectorType.ECAL,DetectorType.FTOF,
+        DetectorType.HTCC,DetectorType.DC,DetectorType.CTOF,
+        DetectorType.CND,DetectorType.BST,DetectorType.RF,
+        DetectorType.BMT,DetectorType.FMT,DetectorType.RICH,
+        DetectorType.HEL,DetectorType.BAND,DetectorType.RTPC,
+        DetectorType.RASTER,DetectorType.ATOF,DetectorType.AHDC};
+   
+    public static final String[] STYPES = new String[]{
+        "/daq/tt/ftcal","/daq/tt/fthodo","/daq/tt/fttrk",
+        "/daq/tt/ltcc","/daq/tt/ec","/daq/tt/ftof",
+        "/daq/tt/htcc","/daq/tt/dc","/daq/tt/ctof",
+        "/daq/tt/cnd","/daq/tt/svt","/daq/tt/rf",
+        "/daq/tt/bmt","/daq/tt/fmt","/daq/tt/rich2",
+        "/daq/tt/hel","/daq/tt/band","/daq/tt/rtpc",
+        "/daq/tt/raster","/daq/tt/atof","/daq/tt/ahdc"};
+
+    public static void main(String[] args) {
+        ConstantsManager conman = new ConstantsManager();
+        conman.init(STYPES);
+        TranslationTable tt = new TranslationTable();
+        for (int i=0; i<STYPES.length; i++)
+            tt.add(TYPES[i],conman.getConstants(18779,STYPES[i]));
+        tt.show();
     }
 }
