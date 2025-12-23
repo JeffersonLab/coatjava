d6ca7c661a59a5f149440001d5ddcd6c96001500
Nathan Baltzell <baltzell@jlab.org>
2025-12-23 17:48:35 -0500

    fill in more adapative methods

--- common-tools/swim-tools/src/main/java/org/jlab/clas/swimtools/AdaptiveSwim.java
+++ common-tools/swim-tools/src/main/java/org/jlab/clas/swimtools/AdaptiveSwim.java
@@ -7,10 +7,13 @@ import org.jlab.geom.prim.Vector3D;
 import cnuphys.adaptiveSwim.geometry.Line;
 import cnuphys.adaptiveSwim.geometry.Point;
 import cnuphys.adaptiveSwim.geometry.Vector;
+import cnuphys.adaptiveSwim.geometry.Sphere;
+import cnuphys.adaptiveSwim.geometry.Cylinder;
 
 import cnuphys.adaptiveSwim.AdaptiveSwimException;
 import cnuphys.adaptiveSwim.AdaptiveSwimResult;
 import cnuphys.adaptiveSwim.AdaptiveSwimmer;
+import cnuphys.adaptiveSwim.geometry.Plane;
 
 import cnuphys.swim.SwimTrajectory;
 
@@ -60,8 +63,8 @@ public class AdaptiveSwim extends ASwim {
         Point a2 = new Point(axisPoint2.x()/100, axisPoint2.y()/100, axisPoint2.z()/100);
         Line centerLine = new Line(a1, a2);
 
-        cnuphys.adaptiveSwim.geometry.Cylinder targetCylinder = new cnuphys.adaptiveSwim.geometry.Cylinder(centerLine, radius);
-        
+        Cylinder targetCylinder = new Cylinder(centerLine, radius);
+
         try {
         
             AdaptiveSwimResult result = new AdaptiveSwimResult(false);
@@ -98,7 +101,7 @@ public class AdaptiveSwim extends ASwim {
         Vector norm = new Vector(n.asUnit().x(), n.asUnit().y(), n.asUnit().z());
         Point point = new Point(p.x()/100, p.y()/100, p.z()/100);
  
-        cnuphys.adaptiveSwim.geometry.Plane targetPlane = new cnuphys.adaptiveSwim.geometry.Plane(norm, point);
+        Plane targetPlane = new Plane(norm, point);
 
         try {
             AdaptiveSwimResult result = new AdaptiveSwimResult(false);
@@ -128,22 +131,73 @@ public class AdaptiveSwim extends ASwim {
 
     @Override
     public double[] SwimToSphere(double Rad) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 
-    @Override
-    public double[] SwimToPlaneBoundary(double d_cm, Vector3D n, int dir) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        double[] value = new double[8];
 
-    @Override
-    public double[] SwimToBeamLine(double xB, double yB) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        // convert to meters:
+ 
+        Sphere targetSphere = new Sphere(new Point(0,0,0), Rad/100);
+
+        try {
+            AdaptiveSwimResult result = new AdaptiveSwimResult(false);
+            
+            PC.AS.swimSphere(_charge, _x0, _y0, _z0, _pTot, _theta, _phi, targetSphere,
+                            accuracy/100, _rMax, stepSize, cnuphys.swim.Swimmer.getEps(), result);
+            
+            if(result.getStatus() == AdaptiveSwimmer.SWIM_SUCCESS) {
+                value[0] = result.getUf()[0] * 100; // convert back to cm
+                value[1] = result.getUf()[1] * 100; // convert back to cm
+                value[2] = result.getUf()[2] * 100; // convert back to cm
+                value[3] = result.getUf()[3] * _pTot; // normalized values
+                value[4] = result.getUf()[4] * _pTot;
+                value[5] = result.getUf()[5] * _pTot;
+                value[6] = result.getFinalS() * 100;
+                value[7] = 0; // Conversion from kG.m to T.cm
+            }
+            else {
+                return null;
+            }
+                    
+        } catch (AdaptiveSwimException e) {
+            e.printStackTrace();
+        }        
+        return value;
     }
 
     @Override
     public double[] SwimToLine(Line3D l) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        double[] value = new double[8];
+
+        // convert to meters:
+        Point a1 = new Point(l.origin().x()/100, l.origin().y()/100, l.origin().z()/100);
+        Point a2 = new Point(l.end().x()/100, l.end().y()/100, l.end().z()/100);
+        Line targetLine = new Line(a1, a2);
+ 
+        try {
+            AdaptiveSwimResult result = new AdaptiveSwimResult(false);
+            
+            PC.AS.swimLine(_charge, _x0, _y0, _z0, _pTot, _theta, _phi, targetLine,
+                            accuracy/100, _rMax, stepSize, cnuphys.swim.Swimmer.getEps(), result);
+            
+            if(result.getStatus() == AdaptiveSwimmer.SWIM_SUCCESS) {
+                value[0] = result.getUf()[0] * 100; // convert back to cm
+                value[1] = result.getUf()[1] * 100; // convert back to cm
+                value[2] = result.getUf()[2] * 100; // convert back to cm
+                value[3] = result.getUf()[3] * _pTot; // normalized values
+                value[4] = result.getUf()[4] * _pTot;
+                value[5] = result.getUf()[5] * _pTot;
+                value[6] = result.getFinalS() * 100;
+                value[7] = 0; // Conversion from kG.m to T.cm
+            }
+            else {
+                return null;
+            }
+                    
+        } catch (AdaptiveSwimException e) {
+            e.printStackTrace();
+        }        
+        return value;
     }
 
     @Override
