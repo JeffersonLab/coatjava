45142f8650816ef0862a5eafce9fb51a0b6a442f
Nathan Baltzell <baltzell@jlab.org>
2026-01-06 12:30:07 -0500

    reformat

--- common-tools/clas-detector/src/main/java/org/jlab/detector/pulse/ModeAHDC.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/pulse/ModeAHDC.java
@@ -10,23 +10,23 @@ import org.jlab.utils.groups.NamedEntry;
 
 /**
  * A new extraction method dedicated to the AHDC signal waveform
- * 
+ *
  * Some blocks of code are inspired by MVTFitter.java and Bonus12 (`createBonusBank()`)
- * 
+ *
  * To do list:
  * - read pedestal from the DB when a baseline cannot be computed
  * - change the definition of ADC to be the max of the peak?
  * - fit waveforms to define arrival time (and charge)
- * 
+ *
  * @author ftouchte, pilleux
  */
 public class ModeAHDC extends HipoExtractor  {
-
+    
     //Parameters, to be read from DB?
     
     //Saturation threshold should be 4095 (2^12-1)
     //But in practice waveforms saturate below it
-    private final short ADC_LIMIT = 3500; 
+    private final short ADC_LIMIT = 3500;
     //number of consecutive samples exceeding threshold to consider a saturation plateau
     private final int consecutiveSaturatingSamples = 3;
     //Sampling time in ns
@@ -44,10 +44,10 @@ public class ModeAHDC extends HipoExtractor  {
     private final float defaultBaseline = 300;
     // dream clock for fine timestamp correction
     private final float dream_clock = 8.0f;
-
+    
     //Waveform and corresponding pulse
     //This is the CURRENT pulse, it is initialized
-    //at every extraction. This is not the cleanest 
+    //at every extraction. This is not the cleanest
     //but it avoids feeding the same arguments to all methods
     private short[] samples;
     private Pulse pulse;
@@ -55,7 +55,7 @@ public class ModeAHDC extends HipoExtractor  {
     private int binMax;
     private int numberOfBins;
     private int effectiveNumberOfBins;
-
+    
     //Waveform types:
     //wfType 6 ⇒ too small (nsamples <= 10)
     //wfType 5 ⇒ decreasing baseline (or leadingEdgeTime fails)
@@ -83,21 +83,21 @@ public class ModeAHDC extends HipoExtractor  {
                 }
             }
         }
-        if (countNotZeros <= 10) assignInvalidType(); 
+        if (countNotZeros <= 10) assignInvalidType();
     }
     
     /**
-    * This method computes the waveform baseline 
-    * from the average of the first few samples
-    */    
+     * This method computes the waveform baseline
+     * from the average of the first few samples
+     */
     public void baselineComputation()
     {
         //Default baseline
         float adcOffset = -99;
-
+        
         //Check if the wf has sufficient length, if not it is invalid
         if (numberOfBins >= this.nBaselineSamples+1){
-        
+            
             //The baseline is the average of the first few samples
             for(int i=0; i<this.nBaselineSamples;i++){
                 adcOffset += this.samples[i];
@@ -109,35 +109,35 @@ public class ModeAHDC extends HipoExtractor  {
     }
     
     /**
-    * This method assigns a new type to the waveform
-    * by checking if another more restrictive type had already
-    * been assigned
-    * 
-    * @param type an int that is the new wf type to be applied
-    */
+     * This method assigns a new type to the waveform
+     * by checking if another more restrictive type had already
+     * been assigned
+     *
+     * @param type an int that is the new wf type to be applied
+     */
     public void assignValidType(int type){
         //We only assign a new type if a more restrictive type was not defined before
         if(type>this.pulse.wftype) this.pulse.wftype = (short) type;
     }
     
     /**
-    * This method assigns an invalid type (-1) to the waveform
-    */
+     * This method assigns an invalid type (-1) to the waveform
+     */
     public void assignInvalidType(){
         this.pulse.wftype = 6;
     }
     
     /**
-    * This method subtracts the baseline, 
-    * computes the max ADC and corresponding time
-    * and the integral of the wf
-    * 
-    * @return an int for status 
-    * 
-    */    
+     * This method subtracts the baseline,
+     * computes the max ADC and corresponding time
+     * and the integral of the wf
+     *
+     * @return an int for status
+     *
+     */
     public int waveformADCProcessing()
     {
-        //int numberOfBins = this.samples.length; 
+        //int numberOfBins = this.samples.length;
         
         //Initialize to dummy values
         this.pulse.adcMax = -99;
@@ -176,18 +176,18 @@ public class ModeAHDC extends HipoExtractor  {
                 //we store the maximum length of consecutive saturating samples
                 if(nsaturating>maxNsaturating) maxNsaturating = nsaturating;
             }
-            else nsaturating = 0;//this sample is not in a row of saturating samples    
+            else nsaturating = 0;//this sample is not in a row of saturating samples
         }
-
-        //Saturating samples: if the number of consecutive saturating samples 
+        
+        //Saturating samples: if the number of consecutive saturating samples
         //is greater than the limit, we consider the wf saturated
         if(maxNsaturating>=this.consecutiveSaturatingSamples){
             this.assignValidType(1);
             //The time is taken as the middle of the plateau
             this.binMax = (startPlateau + endPlateau)/2;
         }
- 
-        //Define the pulse time as the peak time 
+        
+        //Define the pulse time as the peak time
         this.pulse.time = (this.binMax + this.time_ZS)*this.samplingTime;
         //If there are 2*npts+1 points around the peak
         //(if the peak is not at an edge of the window)
@@ -202,19 +202,19 @@ public class ModeAHDC extends HipoExtractor  {
         }
         
         return 0;
-       }
+    }
     
     /**
-    * This method computes the leading edge time
-    * and time over threshold
-    * over a constant fraction of the peak value
-    * 
-    * @return an int for status
-    * 
-    */    
+     * This method computes the leading edge time
+     * and time over threshold
+     * over a constant fraction of the peak value
+     *
+     * @return an int for status
+     *
+     */
     public int waveformCFAprocessing(){
         
-        //Initialization 
+        //Initialization
         this.pulse.leadingEdgeTime = -9999;
         this.pulse.trailingEdgeTime = -9999;
         this.pulse.timeOverThreshold = -9999;
@@ -231,7 +231,7 @@ public class ModeAHDC extends HipoExtractor  {
         //If the waveform does not cross the ths before the peak
         //it was early or remant from a previous event and we cannot define a leading time
         //we set the time at zero
-        if(binRise==-99) { 
+        if(binRise==-99) {
             this.assignValidType(5);
             this.pulse.leadingEdgeTime = (0 + this.time_ZS)*this.samplingTime;
         }
@@ -249,10 +249,10 @@ public class ModeAHDC extends HipoExtractor  {
         //Crossing the threshold back down after the peak
         int binFall = -99;
         for (int bin = binMax; bin < numberOfBins-1; bin++){
-            if (this.samples[bin] > threshold 
-                    && this.samples[bin+1] <= threshold 
-                    && this.samples[bin]>0 
-                    && this.samples[bin+1]>0){
+            if (this.samples[bin] > threshold
+                && this.samples[bin+1] <= threshold
+                && this.samples[bin]>0
+                && this.samples[bin+1]>0){
                 binFall = bin+1;
                 break; //We keep only the first time the signal crosses back the threshold
             }
@@ -261,87 +261,85 @@ public class ModeAHDC extends HipoExtractor  {
         //it was late and falling edge cannot be defined
         //the time correspond to the end of the signal
         if(binFall==-99) {
-            this.assignValidType(2); 
+            this.assignValidType(2);
             this.pulse.trailingEdgeTime = (this.effectiveNumberOfBins -1 + this.time_ZS)*this.samplingTime;
         }
         else
         {
-          float slopeFall = 0;
-          if (binFall - 1 >= 0)
-            slopeFall = this.samples[binFall] - this.samples[binFall-1];
-          float fittedBinFall = (slopeFall == 0) ? binFall : binFall-1 + (threshold - this.samples[binFall-1])/slopeFall;
-          this.pulse.trailingEdgeTime = (fittedBinFall + this.time_ZS)*this.samplingTime;
+            float slopeFall = 0;
+            if (binFall - 1 >= 0)
+                slopeFall = this.samples[binFall] - this.samples[binFall-1];
+            float fittedBinFall = (slopeFall == 0) ? binFall : binFall-1 + (threshold - this.samples[binFall-1])/slopeFall;
+            this.pulse.trailingEdgeTime = (fittedBinFall + this.time_ZS)*this.samplingTime;
         }
- 
+        
         this.pulse.timeOverThreshold = this.pulse.trailingEdgeTime - this.pulse.leadingEdgeTime;
         //if ((this.pulse.timeOverThreshold < 300) || (this.pulse.timeOverThreshold > 750)) this.assignValidType(4); // bad tot
         if (this.pulse.timeOverThreshold < 300) this.assignValidType(4); // tot too small
         
         return 0;
-        }
+    }
     
-         /**
-         * This methods extracts a time using the Constant Fraction Discriminator (CFD) algorithm
-         * as described in https://commons.wikimedia.org/wiki/File:CFD_Diagram1.jpg for example
-         * 
-         * @return an int for status
-         * 
-         */
-        public int computeTimeUsingConstantFractionDiscriminator(){    
-            //Dummy values for hits for which the leading edge is not defined
-            this.pulse.constantFractionTime = -99;
-            if(this.pulse.wftype>=4) return 0;
-            
-            //int numberOfBins = this.samples.length;
-            float[] signal = new float[numberOfBins];
-
-            // signal generation
-            for (int bin = 0; bin < numberOfBins; bin++){
-                signal[bin] = (1 - this.fractionCFD)*this.samples[bin]; // we fill it with a fraction of the original signal
-                if (bin < numberOfBins - this.binDelayCFD)
-                    signal[bin] += -1*this.fractionCFD*this.samples[bin + this.binDelayCFD]; // we advance and invert a complementary fraction of the original signal and superimpose it to the previous signal
-            }
-            // determine the two humps
-            int binHumpSup = 0;
-            int binHumpInf = 0;
-            for (int bin = 0; bin < numberOfBins; bin++){
-                if (signal[bin] > signal[binHumpSup])
-                    binHumpSup = bin;
-            }
-            for (int bin = 0; bin < binHumpSup; bin++){ // this loop has been added to be sure : binHumpInf < binHumpSup
-                if (signal[bin] < signal[binHumpInf])
-                    binHumpInf = bin;
-            }
-            // research for zero
-            int binZero = 0;
-            for (int bin = binHumpInf; bin <= binHumpSup; bin++){
-                if (signal[bin] < 0)
-                    binZero = bin; // last pass below zero
-            } // at this stage : binZero < constantFractionTime/samplingTime <= binZero + 1 // constantFractionTime is determined by assuming a linear fit between binZero and binZero + 1
-            float slopeCFD = 0;
-            if (binZero + 1 <= numberOfBins)
-                slopeCFD = signal[binZero+1] - signal[binZero];
-            float fittedBinZero = (slopeCFD == 0) ? binZero : binZero + (0 - signal[binZero])/slopeCFD;
-            this.pulse.constantFractionTime = (fittedBinZero + this.time_ZS)*this.samplingTime;
-            
-            return 0;
+    /**
+     * This methods extracts a time using the Constant Fraction Discriminator (CFD) algorithm
+     * as described in https://commons.wikimedia.org/wiki/File:CFD_Diagram1.jpg for example
+     *
+     * @return an int for status
+     *
+     */
+    public int computeTimeUsingConstantFractionDiscriminator(){
+        //Dummy values for hits for which the leading edge is not defined
+        this.pulse.constantFractionTime = -99;
+        if(this.pulse.wftype>=4) return 0;
+        
+        //int numberOfBins = this.samples.length;
+        float[] signal = new float[numberOfBins];
+        
+        // signal generation
+        for (int bin = 0; bin < numberOfBins; bin++){
+            signal[bin] = (1 - this.fractionCFD)*this.samples[bin]; // we fill it with a fraction of the original signal
+            if (bin < numberOfBins - this.binDelayCFD)
+                signal[bin] += -1*this.fractionCFD*this.samples[bin + this.binDelayCFD]; // we advance and invert a complementary fraction of the original signal and superimpose it to the previous signal
         }
-
-	/** 
-	 * Apply fine timestamp correction
-	 *
-	 * adapted from decode/MVTFitter.java
-	 *
-	 * @param timestamp for fine time correction
-	 * @param fineTimeStampResolution correspond to the dream clock (usually equals to 8; but to be checked!)
-	 */
-	
-	private void fineTimeStampCorrection(long timestamp, float fineTimeStampResolution) {
-		long fineTimeStamp = timestamp & 0x00000007; // keep and convert last 3 bits of binary timestamp
-		this.pulse.leadingEdgeTime += (double) ((fineTimeStamp+0.5) * fineTimeStampResolution); //fineTimeStampCorrection, only on the leadingEdgeTime for the moment (we don't use timeMax or constantFractionTime in the reconstruction yet)
-	}
-
-
+        // determine the two humps
+        int binHumpSup = 0;
+        int binHumpInf = 0;
+        for (int bin = 0; bin < numberOfBins; bin++){
+            if (signal[bin] > signal[binHumpSup])
+                binHumpSup = bin;
+        }
+        for (int bin = 0; bin < binHumpSup; bin++){ // this loop has been added to be sure : binHumpInf < binHumpSup
+            if (signal[bin] < signal[binHumpInf])
+                binHumpInf = bin;
+        }
+        // research for zero
+        int binZero = 0;
+        for (int bin = binHumpInf; bin <= binHumpSup; bin++){
+            if (signal[bin] < 0)
+                binZero = bin; // last pass below zero
+        } // at this stage : binZero < constantFractionTime/samplingTime <= binZero + 1 // constantFractionTime is determined by assuming a linear fit between binZero and binZero + 1
+        float slopeCFD = 0;
+        if (binZero + 1 <= numberOfBins)
+            slopeCFD = signal[binZero+1] - signal[binZero];
+        float fittedBinZero = (slopeCFD == 0) ? binZero : binZero + (0 - signal[binZero])/slopeCFD;
+        this.pulse.constantFractionTime = (fittedBinZero + this.time_ZS)*this.samplingTime;
+        
+        return 0;
+    }
+    
+    /**
+     * Apply fine timestamp correction
+     *
+     * adapted from decode/MVTFitter.java
+     *
+     * @param timestamp for fine time correction
+     * @param fineTimeStampResolution correspond to the dream clock (usually equals to 8; but to be checked!)
+     */
+    
+    private void fineTimeStampCorrection(long timestamp, float fineTimeStampResolution) {
+        long fineTimeStamp = timestamp & 0x00000007; // keep and convert last 3 bits of binary timestamp
+        this.pulse.leadingEdgeTime += (double) ((fineTimeStamp+0.5) * fineTimeStampResolution); //fineTimeStampCorrection, only on the leadingEdgeTime for the moment (we don't use timeMax or constantFractionTime in the reconstruction yet)
+    }
     
     /**
      * This method extracts relevant information from the waveform
@@ -356,7 +354,7 @@ public class ModeAHDC extends HipoExtractor  {
     @Override
     public List<Pulse> extract(NamedEntry pars, int id, long timestamp, long time_ZS, short... samples){
         
-        //Initialize everything each time a new wf is read 
+        //Initialize everything each time a new wf is read
         //and new adc information is extracted.
         //This is messy but avoids feeding the same arguments to all methods
         this.pulse = new Pulse();
@@ -367,12 +365,12 @@ public class ModeAHDC extends HipoExtractor  {
         this.binMax = -99;
         
         List<Pulse> output = new ArrayList<>();
-       
+        
         // Pre Process
         this.preProcess();
         //Baseline computation
         this.baselineComputation();
-       
+        
         //Get the ADC information from the pulse (peak ADC and time, integral)
         this.waveformADCProcessing();
         
@@ -381,14 +379,14 @@ public class ModeAHDC extends HipoExtractor  {
         
         //Get the CFD time
         this.computeTimeUsingConstantFractionDiscriminator();
-
-	// Fine timestamp correction on leadingEdgeTime
-	this.fineTimeStampCorrection(timestamp, dream_clock);
+        
+        // Fine timestamp correction on leadingEdgeTime
+        this.fineTimeStampCorrection(timestamp, dream_clock);
         
         output.add(this.pulse);
         return output;
     }
-
+    
     @Override
     public void update(int n, IndexedTable it, DataEvent event, String wfBankName, String adcBankName) {
         DataBank wf = event.getBank(wfBankName);
@@ -412,26 +410,26 @@ public class ModeAHDC extends HipoExtractor  {
             }
         }
     }
-
+    
     @Override
     protected void update(int n, IndexedTable it, Bank wfBank, Bank adcBank) {
-        if (wfBank.getRows() > 0) { 
-            List<Pulse> pulses = getPulses(n, it, wfBank); 
-            adcBank.reset(); 
-            adcBank.setRows(pulses!=null ? pulses.size() : 0); 
-            if (pulses!=null && !pulses.isEmpty()) { 
-                for (int i=0; i<pulses.size(); ++i) { 
-                    copyIndices(wfBank, adcBank, pulses.get(i).id, i); 
-                    adcBank.putInt("ADC", i, (int)pulses.get(i).adcMax); 
-                    adcBank.putFloat("time", i, pulses.get(i).time); 
-                    adcBank.putFloat("leadingEdgeTime", i, pulses.get(i).leadingEdgeTime); 
-                    adcBank.putFloat("timeOverThreshold", i, pulses.get(i).timeOverThreshold); 
-                    adcBank.putFloat("constantFractionTime", i, pulses.get(i).constantFractionTime); 
-                    adcBank.putInt("integral", i, (int)pulses.get(i).integral); 
+        if (wfBank.getRows() > 0) {
+            List<Pulse> pulses = getPulses(n, it, wfBank);
+            adcBank.reset();
+            adcBank.setRows(pulses!=null ? pulses.size() : 0);
+            if (pulses!=null && !pulses.isEmpty()) {
+                for (int i=0; i<pulses.size(); ++i) {
+                    copyIndices(wfBank, adcBank, pulses.get(i).id, i);
+                    adcBank.putInt("ADC", i, (int)pulses.get(i).adcMax);
+                    adcBank.putFloat("time", i, pulses.get(i).time);
+                    adcBank.putFloat("leadingEdgeTime", i, pulses.get(i).leadingEdgeTime);
+                    adcBank.putFloat("timeOverThreshold", i, pulses.get(i).timeOverThreshold);
+                    adcBank.putFloat("constantFractionTime", i, pulses.get(i).constantFractionTime);
+                    adcBank.putInt("integral", i, (int)pulses.get(i).integral);
                     adcBank.putFloat("ped", i, pulses.get(i).pedestal);
-		    adcBank.putShort("wfType", i, pulses.get(i).wftype);
-                } 
+                    adcBank.putShort("wfType", i, pulses.get(i).wftype);
+                }
             }
-        } 
-    }   
+        }
+    }
 }
