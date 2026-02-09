1cb9862a36c33f4d7f692e60acf90a2bf0e99d9b
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-02-09 15:46:17 -0500

    build: automate dependency analysis (#1101)

--- libexec/dependency-analysis.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# analyze maven dependencies
-# NOTE: skips `coat-libs`, since shaded JAR dependencies are "unused" according to `dependency:analyze`
-set -euo pipefail
-mvn dependency:analyze -DfailOnWarning=true -pl '!org.jlab.coat:coat-libs' --no-transfer-progress
