00d78ef2f6d1e078434091b017eb33f1ce6aecfe
Uditha Weerasinghe <92590276+skuditha@users.noreply.github.com>
2026-02-20 20:26:56 -0500

    feat: ALERT AI Pre PID (#1112)
    * modified recobankwriter with dummy functions
    
    * added a call for AI PID --currently in testing mode
    
    * commented lines out for testing
    
    * commented lines out for testing
    
    * modified to include PrePID similar to TrackMatching
    
    * modified to include PrePID similar to TrackMatching
    
    * created PrePIDResult.java
    
    * created ModelPrePID.java
    
    * added ALERT PID bank to bankdefs
    
    * added ALERT PrePID bank to bankdefs
    
    * cleaning up
    
    * cleaning up
    
    * fix: added probabilities for prepid
    
    * fixed a typo in alert.json
    
    * fix: changed all prints to logger + minor cleanup
    
    * fix: added info to alert.json for AI PID bank
    
    * major change: switched from ATOF::clusters to ATOF::hits
    
    ---------
    
    Co-authored-by: Uditha Weerasinghe <skuditha@jlab.org>

---
+++ reconstruction/alert/src/main/java/org/jlab/rec/alert/AIPID/PrePIDResult.java
@@ -0,0 +1,19 @@
+package org.jlab.rec.alert.AIPID;
+
+public class PrePIDResult {
+    public final int trackid;
+    public final int clusterid;
+    public final int prepid;
+    public final float p2212, p45, p46, p47, p49;
+
+    public PrePIDResult(int trackid, int clusterid, int prepid, float p2212, float p45, float p46, float p47, float p49) {
+        this.trackid = trackid;
+        this.clusterid = clusterid;
+        this.prepid = prepid;
+        this.p2212 = p2212;
+        this.p45 = p45;
+        this.p46 = p46;
+        this.p47 = p47;
+        this.p49 = p49;
+    }
+}
