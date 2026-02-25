cfafe55fe4ad828052cfd04a3bc15c1ee21aab6f
Felix Touchte Codjo <119527892+ftouchte@users.noreply.github.com>
2026-02-25 15:00:44 +0100

    fix fully qualified name (#1125)

--- reconstruction/alert/src/main/java/org/jlab/rec/ahdc/HelixFit/HelixFitJava.java
+++ reconstruction/alert/src/main/java/org/jlab/rec/ahdc/HelixFit/HelixFitJava.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.jlab.rec.ahdc.Cluster.DocaCluster;
+import org.jlab.rec.ahdc.DocaCluster.DocaCluster;
 
 /** Helix Fit.
  *
