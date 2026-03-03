b084947ddb8b7c426468230a9fa0673b119183dc
Raffaella De Vita <devita@ge.infn.it>
2026-03-03 15:37:44 -0500

    fix strip numbering and orientation

--- common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/MPGD/trapezoid/MPGDTrapezoidStripFactory.java
+++ common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/MPGD/trapezoid/MPGDTrapezoidStripFactory.java
@@ -336,7 +336,7 @@ public abstract class MPGDTrapezoidStripFactory {
         Vector3d nXY = new Vector3d(ca, -sa, 0.0);
         double rhs = (internalIndex + 0.5) * c.pitch;
 
-        List<Vector3d> inters = new ArrayList<>(4);
+        List<Vector3d> inters = new ArrayList<>();
 
         for (Vector3d[] e : tr.edges()) {
             Vector3d P = new Vector3d(0, 0, 0);
@@ -359,8 +359,14 @@ public abstract class MPGDTrapezoidStripFactory {
                 double d2 = dx * dx + dy * dy;
                 if (d2 > best) {
                     best = d2;
-                    a = inters.get(i);
-                    b = inters.get(j);
+                    if(dy>0) {
+                        a = inters.get(i);
+                        b = inters.get(j);
+                    }
+                    else {
+                        a = inters.get(j);
+                        b = inters.get(i);                    
+                    }
                 }
             }
         }
@@ -418,9 +424,9 @@ public abstract class MPGDTrapezoidStripFactory {
             tmp.add(s);
         }
 
-        tmp.sort(Comparator
-                .comparingDouble((StripGeom s) -> s.orderXLocal)
-                .thenComparingInt(s -> s.internalIndex));
+//        tmp.sort(Comparator
+//                .comparingDouble((StripGeom s) -> s.orderXLocal)
+//                .thenComparingInt(s -> s.internalIndex));
 
         int comp = 1;
         for (StripGeom s : tmp) {
