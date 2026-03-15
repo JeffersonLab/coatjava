2ea1b57a34939d727bb8f89451a982629c2578e7
Nathan Baltzell <baltzell@gmx.com>
2026-03-14 21:51:10 -0400

    cleanup eb test script

--- validation/advanced-tests/run-eb-tests.sh
+++ validation/advanced-tests/run-eb-tests.sh
@@ -1,162 +1,63 @@
 #!/bin/bash
 
-webDir=http://clasweb.jlab.org/clas12offline/distribution/coatjava/validation_files/eb
-webVersion=5.11-fid-tm-dc2-r11
-webDir=$webDir/$webVersion
-webDir=./data/$webVersion
-
 # coatjava must already be built at ../../coatjava/
 # and input test data files at ./data
 
-# whether to use CLARA (0=no)
-useClara=0
+#set -x # print every command executed
 
-# if non-zero, don't redownload dependencies, don't run reconstruction:
-runTestOnly=0
+input_dir=./data/5.11-fid-tm-dc2-r11
 
-# gemc default solenoid (changed in 4a.2.4):
-gemcSolenoidDefault=-1.0
-if [[ $webVersion = *"4a.2.2"* ]] || [[ $webVersion = *"4a.2.3"* ]]
-then
-    gemcSolenoidDefault=1.0
-fi
+source ../../coatjava/libexec/env.sh
 
-# geometry variation for DC
-geoDbVariation="default"
-if [[ $webVersion = *"4a.2.2"* ]] || [[ $webVersion = *"4a.2.3"* ]] || [[ $webVersion = *"4a.2.4"* ]]
-then
-    geoDbVariation="dc_geo_gemc424"
-fi
+classPath="${COATJAVA_CLASSPATH}:../lib/*:src/"
 
-nEvents=-1
-
+# check arguments:
 for arg in $@
 do
-    if [ "$arg" == "-t" ]
+    if [[ $arg == "-100" ]]
     then
-        runTestOnly=1
-    elif [[ $arg == "-100" ]]
-    then
-        webDir=${webDir}-100
+        input_dir=${input_dir}-100
     fi
 done
 
 # last argument is input file stub:
 stub="${@: -1}"
 
-# sanity check on filestub name,
-# just to error with reasonable message before proceeding:
+# check (file)stub name:
 case $stub in
-    # electron in forward, hadron in forward:
-    electronproton)
-        ;;
-    electronkaon)
-        ;;
-    electronpion)
-        ;;
-    electrongamma)
-        ;;
-    electronneutron)
-        ;;
-    electronFTproton)
-        ;;
-    electronFTkaon)
-        ;;
-    electronFTpion)
-        ;;
-    electronFTgamma)
-        ;;
-    electrongammaFT)
-        ;;
-    electronprotonC)
-        ;;
-    electronkaonC)
-        ;;
-    electronpionC)
-        ;;
-    electrongammaC)
-        ;;
-    electronneutronC)
-        ;;
-    electrondeuteronC)
-        ;;
+    electronproton) ;;
+    electronkaon) ;;
+    electronpion) ;;
+    electrongamma) ;;
+    electronneutron) ;;
+    electronFTproton) ;;
+    electronFTkaon) ;;
+    electronFTpion) ;;
+    electronFTgamma) ;;
+    electrongammaFT) ;;
+    electronprotonC) ;;
+    electronkaonC) ;;
+    electronpionC) ;;
+    electrongammaC) ;;
+    electronneutronC) ;;
+    electrondeuteronC) ;;
     *)
       echo Invalid input evio file:  $stub
       exit 1
 esac
 
-# set up environment
-if [ $useClara -eq 0 ]
-then
-    COAT=../../coatjava
-    source $COAT/libexec/env.sh
-else
-    CLARA_HOME=$PWD/clara_installation/
-    COAT=$CLARA_HOME/plugins/clas12/
-    source $COAT/libexec/env.sh
-    export CLARA_HOME
-fi
-
-classPath="${COATJAVA_CLASSPATH}:../lib/*:src/"
-
-# make sure test code compiles before anything else:
+# compile test code:
 javac -cp $classPath src/eb/EBTwoTrackTest.java
 if [ $? != 0 ] ; then echo "EBTwoTrackTest compilation failure" ; exit 1 ; fi
 
-# download and setup dependencies, run reconstruction:
-if [ $runTestOnly -eq 0 ]
-then
+# run reconstruction:
+rm -f out_${stub}.hipo
+../../coatjava/bin/recon-util -l FINE -i ${input_dir}/${stub}.hipo -o out_${stub}.hipo -c 2
 
-    if ! [ $useClara -eq 0 ]
-    then
-        # tar the local coatjava build so it can be installed with clara
-        cd ../..
-        tar -zcvf coatjava-local.tar.gz coatjava
-        mv coatjava-local.tar.gz validation/advanced-tests/
-        cd -
-
-        # install clara
-        if ! [ -d clara_installation ]
-        then
-            ../../bin/install-clara clara_installation
-        fi
-    fi
-
-    # download test files, if necessary:
-    #wget -N --no-check-certificate $webDir/${stub}.hipo
-    #if [ $? != 0 ] ; then echo "wget validation files failure" ; exit 1 ; fi
-
-    # update the schema dictionary:  (no longer necessary now that recon-util does it)
-    #rm -f up_${stub}.hipo
-    #../../coatjava/bin/hipo-utils -update -d ../../coatjava/etc/bankdefs/hipo4/ -o up_${stub}.hipo ${stub}.hipo
-
-    # run reconstruction:
-    rm -f out_${stub}.hipo
-    if [ $useClara -eq 0 ]
-    then
-        GEOMDBVAR=$geoDbVariation
-        export GEOMDBVAR
-        ../../coatjava/bin/recon-util -l FINE -i ${webDir}/${stub}.hipo -o out_${stub}.hipo -c 2
-    else
-        echo "set inputDir $PWD/" > cook.clara
-        echo "set outputDir $PWD/" >> cook.clara
-        echo "set threads 7" >> cook.clara
-        echo "set javaMemory 2" >> cook.clara
-        echo "set session s_cook" >> cook.clara
-        echo "set description d_cook" >> cook.clara
-        ls ${stub}.hipo > files.list
-        echo "set fileList $PWD/files.list" >> cook.clara
-        echo "run local" >> cook.clara
-        echo "exit" >> cook.clara
-        $CLARA_HOME/bin/clara-shell cook.clara
-    fi
-fi
-
-# run Event Builder tests:
+# run EB tests:
 java -DCLAS12DIR="$COAT" -Xmx1536m -Xms1024m -cp $classPath -DINPUTFILE=out_${stub}.hipo org.junit.runner.JUnitCore eb.EBTwoTrackTest
 if [ $? != 0 ] ; then echo "EBTwoTrackTest unit test failure" ; exit 1 ; else echo "EBTwoTrackTest passed unit tests" ; fi
 
+# run truth-efficiency calculator:
 $COAT/bin/trutheff ./out_${stub}.hipo
-
-exit 0
 
