6f6f0e754ac848aa2315aff27b80d18b683fda00
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-04-07 14:26:14 -0400

    fix: cherry pick generalized rollover correction

--- common-tools/clas-detector/src/main/java/org/jlab/detector/scalers/DaqScalersSequence.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/scalers/DaqScalersSequence.java
@@ -362,18 +362,51 @@ public class DaqScalersSequence implements Comparator<DaqScalers> {
      */
     public void fixClockRollover() {
         boolean modified = true;
+        // ungated clock
         while (modified) {
             modified = false;
             for (int i=this.scalers.size()-1; i>0; --i) {
                 Dsc2Scaler previous = this.scalers.get(i-1).dsc2;
-                Dsc2Scaler next = this.scalers.get(i).dsc2;
-                if (previous.clock > next.clock) {
+                Dsc2Scaler next     = this.scalers.get(i).dsc2;
+                long corr           = previous.clock - next.clock + 1;
+                boolean is_rollover = previous.clock > next.clock;
+                boolean is_gap      = corr <= -2*(long)Integer.MAX_VALUE;
+                if (is_rollover || is_gap) {
+                    if(is_gap) corr = -corr;
                     for (int j=i; j<this.scalers.size(); ++j) {
-                        if (j==i) System.out.print( String.format("FIXING CLOCK ROLLOVER:  %d %d -> ",this.scalers.get(j).dsc2.clock,this.scalers.get(j).dsc2.gatedClock));
-                        this.scalers.get(j).dsc2.clock += 2*(long)Integer.MAX_VALUE;
-                        // The gated clock also rolls over (but it's triggered by the ungated clock, not itself!?):
-                        this.scalers.get(j).dsc2.gatedClock += 2*(long)Integer.MAX_VALUE;
-                        if (j==i) System.out.println(String.format("%d %d",this.scalers.get(j).dsc2.clock,this.scalers.get(j).dsc2.gatedClock));
+                        if (j==i) System.out.print( String.format("FIXING UNGATED CLOCK ROLLOVER:  %d -> ",this.scalers.get(j).dsc2.clock));
+                        this.scalers.get(j).dsc2.clock += corr;
+                        if (j==i) {
+                          System.out.println(String.format("%d",this.scalers.get(j).dsc2.clock));
+                          System.out.println((double)corr/(2*(long)Integer.MAX_VALUE));
+                          if(is_gap) System.out.println("GAP!");
+                        }
+                    }
+                    modified = true;
+                    break;
+                }
+            }
+        }
+        // repeat for gated clock
+        modified = true;
+        while (modified) {
+            modified = false;
+            for (int i=this.scalers.size()-1; i>0; --i) {
+                Dsc2Scaler previous = this.scalers.get(i-1).dsc2;
+                Dsc2Scaler next     = this.scalers.get(i).dsc2;
+                long corr           = previous.gatedClock - next.gatedClock + 1;
+                boolean is_rollover = previous.gatedClock > next.gatedClock;
+                boolean is_gap      = corr <= -2*(long)Integer.MAX_VALUE;
+                if (is_rollover || is_gap) {
+                    if(is_gap) corr = -corr;
+                    for (int j=i; j<this.scalers.size(); ++j) {
+                        if (j==i) System.out.print( String.format("FIXING GATED CLOCK ROLLOVER:  %d -> ",this.scalers.get(j).dsc2.gatedClock));
+                        this.scalers.get(j).dsc2.gatedClock += corr;
+                        if (j==i) {
+                          System.out.println(String.format("%d",this.scalers.get(j).dsc2.gatedClock));
+                          System.out.println((double)corr/(2*(long)Integer.MAX_VALUE));
+                          if(is_gap) System.out.println("GAP!");
+                        }
                     }
                     modified = true;
                     break;
