560c22c4357518fbe1dbf80d6bd082df37ead9a0
Nathan Baltzell <baltzell@gmx.com>
2026-08-24 12:55:40 -0400

    cleanup benchmark printouts (#1394)

--- common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/BenchmarkTimer.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/BenchmarkTimer.java
@@ -58,7 +58,7 @@ public class BenchmarkTimer {
     public String toString() {
         double timePerCall = 0.0;
         if (numberOfCalls != 0) timePerCall = getMiliseconds() / numberOfCalls;
-        return String.format("TIMER (%-12s) : N Calls %12d,  Total Time  = %12.2f sec,  Unit Time = %12.3f msec",
+        return String.format("%-15s : #Calls %12d, Total = %12.2f sec, Unit = %12.3f msec",
             getName(), numberOfCalls, getSeconds(), timePerCall);
     }
 }
