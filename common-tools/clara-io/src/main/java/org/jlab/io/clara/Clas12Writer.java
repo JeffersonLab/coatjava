87fdeb09a491196493fdf5ec7d9721e4138ba9bb
Nathan Baltzell <baltzell@jlab.org>
2026-08-27 21:13:09 -0400

    cleanup

--- common-tools/clara-io/src/main/java/org/jlab/io/clara/Clas12Writer.java
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/Clas12Writer.java
@@ -33,6 +33,13 @@ import org.json.JSONObject;
  */
 public class Clas12Writer extends HipoToHipoWriter {
 
+    public static class Poster {
+        TreeMap<Integer,Integer> eventUnix;
+        TreeSet<HelicityState> helicities;
+        DaqScalersSequence scalers;
+        void process(Event e) {}
+    }
+    
     static final String[] TAG1BANKS = {"RUN::scaler","HEL::scaler","RAW::scaler","RAW::epics","HEL::flip","COAT::config"};
 
     Bank[] tag1banks;
