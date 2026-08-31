4839c1a96aad7d3eaa047024aa3d44a68aca3ed8
Nathan Baltzell <baltzell@gmx.com>
2026-08-31 16:08:35 -0400

    add a warmup

--- common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/ProgressPrintout.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/ProgressPrintout.java
@@ -10,6 +10,8 @@ import java.util.logging.Logger;
  * @author gavalian
  */
 public class ProgressPrintout {
+
+    private final int WARMUP_CALLS = 100;
     
     private TreeMap<String,Object> items   = new TreeMap<>();
     private Long     previousPrintoutTime = (long) 0;
@@ -36,8 +38,8 @@ public class ProgressPrintout {
     public String getUpdateString(){
         double totalElapsedTime = (this.previousPrintoutTime-this.startPrintoutTime)*1e-3;
         StringBuilder str = new StringBuilder();
-        double averageTime = 1000.0*totalElapsedTime/this.numberOfCalls;
-        str.append(String.format("%s (%12d) : ", this.printoutLeadingString,this.numberOfCalls));
+        double averageTime = 1000.0*totalElapsedTime/(this.numberOfCalls-WARMUP_CALLS);
+        str.append(String.format("%s (%12d) : ", this.printoutLeadingString,this.numberOfCalls-WARMUP_CALLS));
         str.append(String.format(" time : %8.2f (sec) =>>> average time = %9.3f msec", totalElapsedTime,averageTime));
         Set<String> keys = this.items.keySet();
         for(String key : keys){
@@ -53,14 +55,17 @@ public class ProgressPrintout {
     }
     
     public void updateStatus(){        
-        this.numberOfCalls++;
-        Long currentTime   = System.currentTimeMillis();
-        Double elapsedTime = (currentTime - this.previousPrintoutTime)*1e-3;
-        //System.out.println("elapsed = " + elapsedTime);
-        if(elapsedTime>=this.printoutIntervalSeconds){
+        if (++this.numberOfCalls < WARMUP_CALLS) {
             this.previousPrintoutTime = System.currentTimeMillis();
-            //System.out.println(" passed time ");
-            System.out.println(this.getUpdateString());
+            this.startPrintoutTime = System.currentTimeMillis();
+        }
+        else {
+            Long currentTime   = System.currentTimeMillis();
+            Double elapsedTime = (currentTime - this.previousPrintoutTime)*1e-3;
+            if(elapsedTime >= this.printoutIntervalSeconds){
+                this.previousPrintoutTime = System.currentTimeMillis();
+                System.out.println(this.getUpdateString());
+            }
         }
     }
     
