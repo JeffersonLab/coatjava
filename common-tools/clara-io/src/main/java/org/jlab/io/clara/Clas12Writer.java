25a4f581503267c4adceb974c95081fb2312f632
Nathan Baltzell <baltzell@gmx.com>
2026-09-04 09:21:43 -0400

    separate "serial" functionality for reusability

--- common-tools/clara-io/src/main/java/org/jlab/io/clara/Clas12Writer.java
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/Clas12Writer.java
@@ -3,16 +3,11 @@ package org.jlab.io.clara;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import org.jlab.analysis.postprocess.Processor;
 import org.jlab.clara.std.services.EventWriterException;
 import org.jlab.detector.calib.utils.ConstantsManager;
-import org.jlab.detector.decode.CLASDecoder4;
-import org.jlab.detector.helicity.HelicitySequence;
 import org.jlab.detector.helicity.HelicitySequenceDelayed;
-import org.jlab.detector.helicity.HelicityState;
-import org.jlab.detector.scalers.DaqScalersSequence;
+import org.jlab.detector.serial.SerialHoncho;
 import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.data.SchemaFactory;
@@ -33,34 +28,22 @@ import org.json.JSONObject;
  */
 public class Clas12Writer extends HipoToHipoWriter {
 
-    static final String[] TAG1BANKS = {"RUN::scaler","HEL::scaler","RAW::scaler","RAW::epics","HEL::flip","COAT::config"};
-
-    Bank[] tag1banks;
+    SerialHoncho serial;
     Bank runConfig;
-    Bank helicityAdc;
     ConstantsManager conman;
-    TreeMap<Integer,Integer> eventUnix;
-    TreeSet<HelicityState> helicities;
-    DaqScalersSequence scalers;
     SchemaFactory fullSchema;
     boolean postprocess;
 
     private void init(JSONObject opts) {
         fullSchema = new SchemaFactory();
         fullSchema.initFromDirectory(FileUtils.getEnvironmentPath("CLAS12DIR","etc/bankdefs/hipo4"));
+        serial = new SerialHoncho(fullSchema);
         runConfig = new Bank(fullSchema.getSchema("RUN::config"));
-        helicityAdc = new Bank(fullSchema.getSchema("HEL::adc"));
-        helicities = new TreeSet<>();
-        scalers = new DaqScalersSequence(fullSchema);
         conman = new ConstantsManager();
-        eventUnix = new TreeMap<>();
         conman.init("/runcontrol/hwp","/runcontrol/helicity");
         postprocess = opts.optBoolean("postprocess", false);
         if (opts.has("variation")) conman.setVariation(opts.getString("variation"));
         if (opts.has("timestamp")) conman.setTimeStamp(opts.getString("timestamp"));
-        tag1banks = new Bank[TAG1BANKS.length];
-        for (int i=0; i<tag1banks.length; ++i)
-            tag1banks[i] = new Bank(fullSchema.getSchema(TAG1BANKS[i]));
     }
 
     @Override
@@ -78,29 +61,17 @@ public class Clas12Writer extends HipoToHipoWriter {
 
     @Override
     protected void writeEvent(Object event) throws EventWriterException {
-        scalers.add((Event)event);
-        ((Event)event).read(runConfig);
-        ((Event)event).read(helicityAdc);
-        if (runConfig.getRows() > 0) {
-            int unix = runConfig.getInt("unixtime",0);
-            int evno = runConfig.getInt("event",0);
-            if (unix > 0 && evno > 0) eventUnix.put(evno, unix);
-        }
-        helicities.add(HelicityState.createFromFadcBank(helicityAdc, runConfig, conman));
-        Event t = CLASDecoder4.createTaggedEvent((Event)event, runConfig, tag1banks);
+        Event t = serial.read((Event)event);
         if (!t.isEmpty()) writer.addEvent(t, 1);
         super.writeEvent(event);
     }
 
     @Override
     protected void closeWriter() {
-        HelicitySequence.writeFlips(fullSchema, writer, helicities);
-        writer.addEvent(getUnixEvent(runConfig),1);
+        serial.finish(writer);
         super.closeWriter();
         if (postprocess) postprocess();
-        // keep the latest helicity/scaler reading for the next file:
-        while (helicities.size() > 60) helicities.pollFirst();
-        scalers.clear(10);
+        serial.clear();
     }
  
     /**
@@ -121,34 +92,13 @@ public class Clas12Writer extends HipoToHipoWriter {
     }
 
     /**
-     * Get a new event with a RUN::unix bank containing event-timestamp mapping,
-     * and the latest RUN::config bank.
-     * @param config
-     * @return 
-     */
-    private Event getUnixEvent(Bank config) {
-        Bank unix = new Bank(fullSchema.getSchema("RUN::unix"));
-        unix.setRows(eventUnix.size());
-        int row = 0;
-        for (int evno : eventUnix.keySet()) {
-            unix.putInt("event", row, evno);
-            unix.putInt("unixtime",row, eventUnix.get(evno));
-            row++;
-        }
-        Event e = new Event();
-        e.write(config);
-        e.write(unix);
-        return e;
-    }
-
-    /**
      * Copy helicity/charge tag-1 information to all events.
      */
     private void postprocess() {
         int d = conman.getConstants(getRunNumber(), "/runcontrol/helicity").getIntValue("delay",0,0,0);
         HelicitySequenceDelayed helicity = new HelicitySequenceDelayed(d);
-        helicity.addStream(helicities);
-        Processor p = new Processor(List.of(filename), fullSchema, helicity, scalers);
+        helicity.addStream(serial.getHelicities());
+        Processor p = new Processor(List.of(filename), fullSchema, helicity, serial.getScalers());
         HipoReader r = new HipoReader();
         r.open(filename);
         Event e = new Event();
