d0c451fd0f6815b95882cbe45e8fbeac18a8a8d7
Nathan Baltzell <baltzell@gmx.com>
2026-09-10 19:00:00 -0400

    remove post-processing from Clas12Writer

--- common-tools/clara-io/src/main/java/org/jlab/io/clara/Clas12Writer.java
+++ common-tools/clara-io/src/main/java/org/jlab/io/clara/Clas12Writer.java
@@ -1,21 +1,11 @@
 package org.jlab.io.clara;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import org.jlab.clara.std.services.EventWriterException;
 import org.jlab.detector.calib.utils.ConstantsManager;
-import org.jlab.detector.helicity.HelicitySequenceDelayed;
-import org.jlab.detector.helicity.HelicityState;
-import org.jlab.detector.scalers.DaqScalersSequence;
-import org.jlab.detector.serial.PostProcessor;
 import org.jlab.detector.serial.SerialHoncho;
-import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.data.SchemaFactory;
-import org.jlab.jnp.hipo4.io.HipoReader;
 import org.jlab.jnp.hipo4.io.HipoWriterSorted;
 import org.jlab.jnp.utils.file.FileUtils;
 import org.json.JSONObject;
@@ -25,7 +15,6 @@ import org.json.JSONObject;
  * 1. Copies certain banks on-the-fly to new tag-1 events
  * 2. Caches helicity states, scaler readouts, and unix time
  * 3. Writes HEL::flip, RUN/HEL::scaler, and RUN::unix to new tag-1 events
- * 4. Runs post-processing, writing tag-1 information to all events 
  * 5. Adds .hipo to the output filename, if necessary
  *
  * @author baltzell
@@ -33,19 +22,15 @@ import org.json.JSONObject;
 public class Clas12Writer extends HipoToHipoWriter {
 
     SerialHoncho serial;
-    Bank runConfig;
     ConstantsManager conman;
     SchemaFactory fullSchema;
-    boolean postprocess;
 
     private void init(JSONObject opts) {
         fullSchema = new SchemaFactory();
         fullSchema.initFromDirectory(FileUtils.getEnvironmentPath("CLAS12DIR","etc/bankdefs/hipo4"));
         serial = new SerialHoncho(fullSchema);
-        runConfig = new Bank(fullSchema.getSchema("RUN::config"));
         conman = new ConstantsManager();
         conman.init("/runcontrol/hwp","/runcontrol/helicity");
-        postprocess = opts.optBoolean("postprocess", false);
         if (opts.has("variation")) conman.setVariation(opts.getString("variation"));
         if (opts.has("timestamp")) conman.setTimeStamp(opts.getString("timestamp"));
     }
@@ -74,47 +59,6 @@ public class Clas12Writer extends HipoToHipoWriter {
     protected void closeWriter() {
         serial.finish(writer);
         super.closeWriter();
-        if (postprocess) postprocess();
         serial.clear();
     }
- 
-    /**
-     * Get the first valid run number from a RUN::config bank.
-     * @return run
-     */
-    private int getRunNumber() {
-        Event e = new Event();
-        HipoReader r = new HipoReader();
-        r.open(filename);
-        while (r.hasNext()) {
-            r.nextEvent(e);
-            e.read(runConfig);
-            if (runConfig.getRows()>0 && runConfig.getInt("run",0)>0)
-                return runConfig.getInt("run",0);
-        }
-        return 0;
-    }
-
-    /**
-     * Copy helicity/charge tag-1 information to all events.
-     */
-    private void postprocess() {
-        int d = conman.getConstants(getRunNumber(), "/runcontrol/helicity").getIntValue("delay",0,0,0);
-        HelicitySequenceDelayed helicity = new HelicitySequenceDelayed(d);
-        helicity.addStream(serial.getHelicities());
-        PostProcessor p = new PostProcessor(List.of(filename), fullSchema, helicity, serial.getScalers());
-        HipoReader r = new HipoReader();
-        r.open(filename);
-        Event e = new Event();
-        writer.open("pp_"+filename);
-        while (r.hasNext()) {
-            r.nextEvent(e);
-            p.processEvent(e);
-            HipoToHipoWriter.writeEvent(writer, e, schemaBankList);
-        }
-        writer.close();
-        new File(filename).delete();
-        new File("pp_"+filename).renameTo(new File(filename));
-    }
-
 }
