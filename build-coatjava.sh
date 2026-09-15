fe9bcc2475ba1dde54c87c24e97dafc9ab7a234e
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-09-15 17:44:24 -0400

    build: don't shade, prefer separate jar

--- build-coatjava.sh
+++ build-coatjava.sh
@@ -395,6 +395,8 @@ done
 for pom in $(find common-tools -name pom.xml); do
   if [[ "$pom" =~ coat-libs ]]; then
     install_jars $pom $prefix_dir/lib/clas 'coat-libs-*.jar'
+  elif [[ "$pom" =~ clas-qcddat ]]; then
+    install_jars $pom $prefix_dir/lib/services
   # else # FIXME, consumers may be need these after https://github.com/JeffersonLab/coatjava/pull/632 ; alternatively add needed deps to `coat-libs`
   #   install_jars $pom $prefix_dir/lib/services
   fi
