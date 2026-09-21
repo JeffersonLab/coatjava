3272556721a4dc6f01f9f9e1f87fc11e9217b290
Nathan Baltzell <baltzell@gmx.com>
2026-09-21 15:56:10 -0400

    fixed precision on benchmark printout (#1445)

--- common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/Benchmark.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/Benchmark.java
@@ -106,8 +106,8 @@ public class Benchmark {
     public String[] toCSV() {
         return new String[]{
             String.join(",",timerStore.keySet()) + ",TOTAL",
-            String.join(",",timerStore.values().stream().map(x -> String.valueOf(x.getMillisecondsPerCall())).toList())
-                + "," + timerStore.values().stream().mapToDouble(x -> x.getMillisecondsPerCall()).sum()
+            String.join(",",timerStore.values().stream().map(x -> String.format("%.2f",x.getMillisecondsPerCall())).toList())
+                + "," + String.format("%.2f",timerStore.values().stream().mapToDouble(x -> x.getMillisecondsPerCall()).sum())
         };
     }
 
