66090446bdbf5ab4c0b7d92f4ffb04ed2de7c3a1
Nathan Baltzell <baltzell@gmx.com>
2026-09-21 10:17:21 -0400

    switch kpp test to recon-mutil

--- validation/advanced-tests/run-advanced-tests.sh
+++ validation/advanced-tests/run-advanced-tests.sh
@@ -11,28 +11,20 @@ COAT=$CLARA_HOME/plugins/clas12/
 source $COAT/libexec/env.sh
 classPath="${COATJAVA_CLASSPATH}:../lib/*:src/"
 
-# download test files
-#wget --no-check-certificate http://clasweb.jlab.org/clas12offline/distribution/coatjava/validation_files/twoTrackEvents_809_raw.evio.tar.gz
-#[ $? -ne 0 ] && echo "wget validation files failure" && exit 2
-#tar -zxvf twoTrackEvents_809_raw.evio.tar.gz
-
-# run decoder
-$COAT/bin/decoder -t -0.5 -s 0.0 -i ./data/twoTrackEvents_809_raw.evio -o ./twoTrackEvents_809.hipo -c 2
-[ $? -ne 0 ] && echo "decoder failure" && exit 3
+# run reconstruction
+$COAT/bin/run-mutil -t 2 -y $COAT/etc/services/kpp.yaml -o rec_twoTrackEvents_809.hipo data/twoTrackEvents_809_raw.evio
+[ $? -ne 0 ] && echo "recon-mutil failure" && exit 1
 
 # take a peek
-$COAT/bin/hipo-utils -stats ./twoTrackEvents_809.hipo
-
-# run clara
-$COAT/bin/run-clara -y $COAT/etc/services/kpp.yaml ./twoTrackEvents_809.hipo
-[ $? -ne 0 ] && echo "reconstruction with clara failure" && exit 4
+$COAT/bin/hipo-utils -stats ./rec_twoTrackEvents_809.hipo
+[ $? -ne 0 ] && echo "recon-utils failure" && exit 2
 
 # compile test codes
 javac -cp $classPath src/kpptracking/KppTrackingTest.java 
-[ $? -ne 0 ] && echo "KppTrackingTest compilation failure" && exit 5
+[ $? -ne 0 ] && echo "KppTrackingTest compilation failure" && exit 3
 
 # run KppTracking junit tests
 java -DCLAS12DIR="$COAT" -Xmx1536m -Xms1024m -cp $classPath org.junit.runner.JUnitCore kpptracking.KppTrackingTest
-[ $? -ne 0 ] && echo "KppTracking unit test failure" && exit 6
+[ $? -ne 0 ] && echo "KppTracking unit test failure" && exit 4
 
 echo "KppTracking passed unit tests"
