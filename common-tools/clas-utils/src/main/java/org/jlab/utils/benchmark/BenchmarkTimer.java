c5c1202aa1f4bfcdb9fa78caf7b78aa72cfc0a84
Nathan Baltzell <baltzell@gmx.com>
2026-09-24 18:33:35 -0400

    give benchmarks a warmup too

--- common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/BenchmarkTimer.java
+++ common-tools/clas-utils/src/main/java/org/jlab/utils/benchmark/BenchmarkTimer.java
@@ -11,6 +11,8 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class BenchmarkTimer {
 
+    private static final int WARMUP_CALLS = 100;
+    
     public static class BenchmarkMultiTimer extends BenchmarkTimer {
         ConcurrentHashMap<Integer,Long> timeAtResume = new ConcurrentHashMap<>();
         ConcurrentHashMap<Integer,Boolean> isPaused = new ConcurrentHashMap<>();
@@ -23,8 +25,8 @@ public class BenchmarkTimer {
         }
         public void pause(int thread) {
             if (!isPaused.get(thread)) {
-                numberOfCalls.incrementAndGet();
-                totalTime.addAndGet(System.nanoTime() - timeAtResume.get(thread));
+                if (numberOfCalls.incrementAndGet() > WARMUP_CALLS)
+                    totalTime.addAndGet(System.nanoTime() - timeAtResume.get(thread));
                 isPaused.put(thread, true);
             }
         }
@@ -62,8 +64,8 @@ public class BenchmarkTimer {
     
     public void pause(){
         if(isPaused==false){
-            totalTime.addAndGet(System.nanoTime() - timeAtResume);
-            numberOfCalls.incrementAndGet();
+            if (numberOfCalls.incrementAndGet() > WARMUP_CALLS)
+                totalTime.addAndGet(System.nanoTime() - timeAtResume);
             isPaused = true;
         }
     }
@@ -84,7 +86,7 @@ public class BenchmarkTimer {
     }
     
     public double getMillisecondsPerCall() {
-        return numberOfCalls.get() > 0 ? getMilliseconds() / numberOfCalls.get() : 0;
+        return numberOfCalls.get() > 0 ? getMilliseconds() / (numberOfCalls.get() - WARMUP_CALLS) : 0;
     }
 
     @Override
