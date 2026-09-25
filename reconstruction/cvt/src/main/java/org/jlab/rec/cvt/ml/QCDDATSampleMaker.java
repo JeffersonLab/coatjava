23d71ef45ca6ca749219020c6b66339524f08a14
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-09-25 16:02:24 -0400

    style: cleanup

--- reconstruction/cvt/src/main/java/org/jlab/rec/cvt/ml/QCDDATSampleMaker.java
+++ reconstruction/cvt/src/main/java/org/jlab/rec/cvt/ml/QCDDATSampleMaker.java
@@ -29,7 +29,7 @@ import org.jlab.rec.cvt.banks.HitReader;
 public class QCDDATSampleMaker extends ReconstructionEngine {
 
     private String svtHitBank;
-    
+
     public QCDDATSampleMaker(String name) {
         super(name, "ziegler", "6.0");
     }
@@ -42,26 +42,26 @@ public class QCDDATSampleMaker extends ReconstructionEngine {
     public void detectorChanged(int run) {}
 
     @Override
-    public boolean init() {   
+    public boolean init() {
         this.initConstantsTables();
         this.registerBanks();
-        return true;    
+        return true;
     }
-    
+
     public void registerBanks() {
         this.setSvtHitBank("CVT::QCDDATHit");
         super.registerOutputBank(this.svtHitBank);
     }
-    
+
     public int getRun(DataEvent event) {
-    
+
         if (event.hasBank("RUN::config") == false) {
             System.err.println("RUN CONDITIONS NOT READ!");
             return 0;
         }
 
         DataBank bank = event.getBank("RUN::config");
-        int run = bank.getInt("run", 0);  
+        int run = bank.getInt("run", 0);
         if(Constants.getInstance().seedingDebugMode) {
             System.out.println("EVENT "+bank.getInt("event", 0));
         }
@@ -70,69 +70,61 @@ public class QCDDATSampleMaker extends ReconstructionEngine {
 
     @Override
     public boolean processDataEventUser(DataEvent event) {
-       
-        int run = this.getRun(event); 
+
+        int run = this.getRun(event);
         Swim swimmer = new Swim();
-        IndexedTable svtStatus          = this.getConstantsManager().getConstants(run, "/calibration/svt/status");
-        IndexedTable svtLorentz         = this.getConstantsManager().getConstants(run, "/calibration/svt/lorentz_angle");
-        IndexedTable bmtStatus          = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_status");
-        IndexedTable bmtTime            = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_time");
-        IndexedTable bmtVoltage         = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_voltage");
-        IndexedTable bmtStripVoltage    = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_strip_voltage");
-        IndexedTable bmtStripThreshold  = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_strip_voltage_thresholds");
-        IndexedTable adcStatus            = this.getConstantsManager().getConstants(run, "/calibration/svt/adcstatus");
-        
+        IndexedTable svtStatus         = this.getConstantsManager().getConstants(run, "/calibration/svt/status");
+        IndexedTable svtLorentz        = this.getConstantsManager().getConstants(run, "/calibration/svt/lorentz_angle");
+        IndexedTable bmtStatus         = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_status");
+        IndexedTable bmtTime           = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_time");
+        IndexedTable bmtVoltage        = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_voltage");
+        IndexedTable bmtStripVoltage   = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_strip_voltage");
+        IndexedTable bmtStripThreshold = this.getConstantsManager().getConstants(run, "/calibration/mvt/bmt_strip_voltage_thresholds");
+        IndexedTable adcStatus         = this.getConstantsManager().getConstants(run, "/calibration/svt/adcstatus");
+
         Geometry.getInstance().initialize(this.getConstantsManager().getVariation(), run, svtLorentz, bmtVoltage);
-        
+
         HitReader hitRead = new HitReader();
         hitRead.fetch_SVTHits(event, -1, -1, svtStatus, adcStatus);
-        hitRead.fetch_BMTHits(event, swimmer, bmtStatus, bmtTime, 
-                  bmtStripVoltage, bmtStripThreshold);
+        hitRead.fetch_BMTHits(event, swimmer, bmtStatus, bmtTime, bmtStripVoltage, bmtStripThreshold);
+
         List<ArrayList<Hit>>  hits = new ArrayList<>();
-        if(hitRead.getSVTHits() == null) {
-            hits.add(new ArrayList<>());
-        }
-        else {
-            hits.add((ArrayList<Hit>) hitRead.getSVTHits());
-        }
-        if(hitRead.getBMTHits() == null) {
-            hits.add(new ArrayList<>());
-        }
-        else {
-            hits.add((ArrayList<Hit>) hitRead.getBMTHits());
-        }
-        
+        hits.add(hitRead.getSVTHits() == null ? new ArrayList<>() : (ArrayList<Hit>) hitRead.getSVTHits());
+        hits.add(hitRead.getBMTHits() == null ? new ArrayList<>() : (ArrayList<Hit>) hitRead.getBMTHits());
+
         if (event.hasBank("MC::True")) {
             DataBank mcTrue = event.getBank("MC::True");
             TrackingPerformance.MatchHitsToMC(hits, mcTrue);
         }
-        
-        if (hits.isEmpty()) 
+
+        if (hits.isEmpty())
             return false;
+
         DataBank bank = event.createBank(this.svtHitBank, hits.get(0).size()+hits.get(1).size());
         int index=0;
-        for(int i = 0; i < hits.size(); i++) {
-            for(int j = 0; j < hits.get(i).size(); j++) {
-                bank.setShort("id", index, (short) hits.get(i).get(j).getId());
-                bank.setShort("mctid", index, (short) hits.get(i).get(j).getAssociateMCTrkId());
-                bank.setByte("sector", index, (byte) hits.get(i).get(j).getSector());
-                int layer = hits.get(i).get(j).getLayer();
+        for (int i = 0; i < hits.size(); i++) {
+            for (int j = 0; j < hits.get(i).size(); j++) {
+
+                var hit = hits.get(i).get(j);
+
+                int layer = hit.getLayer();
                 if(i>0) layer+=6;
-                bank.setByte("layer", index, (byte) layer);
-                bank.setShort("strip", index, (short) hits.get(i).get(j).getStrip().getStrip());
-                bank.setFloat("energy", index, (short) hits.get(i).get(j).getStrip().getEdep());
-                bank.setFloat("time", index, (short) hits.get(i).get(j).getStrip().getTime());
-                int mctrue=-1;
-                if(hits.get(i).get(j).MCstatus==0) {
-                    mctrue=0;
-                } else {
-                    mctrue=1;
-                }
-                bank.setByte("mctrue", index, (byte) mctrue);
-                if(hits.get(i).get(j).getDetector()==DetectorType.BST ||
-                        (hits.get(i).get(j).getDetector()==DetectorType.BMT 
-                        && hits.get(i).get(j).getType()==BMTType.Z)) {
-                    Line3D sline = hits.get(i).get(j).getStrip().getLine();
+
+                int mctrue = hit.MCstatus==0 ? 0 : 1;
+
+                bank.setShort( "id",     index, (short) hit.getId()               );
+                bank.setShort( "mctid",  index, (short) hit.getAssociateMCTrkId() );
+                bank.setByte(  "sector", index, (byte)  hit.getSector()           );
+                bank.setByte(  "layer",  index, (byte)  layer                     );
+                bank.setShort( "strip",  index, (short) hit.getStrip().getStrip() );
+                bank.setFloat( "energy", index, (short) hit.getStrip().getEdep()  );
+                bank.setFloat( "time",   index, (short) hit.getStrip().getTime()  );
+                bank.setByte(  "mctrue", index, (byte)  mctrue                    );
+
+                if (  hit.getDetector()==DetectorType.BST ||
+                     (hit.getDetector()==DetectorType.BMT && hit.getType()==BMTType.Z))
+                {
+                    Line3D sline = hit.getStrip().getLine();
                     bank.setFloat("x1",   index,  (float) sline.origin().x()/10);
                     bank.setFloat("y1",   index,  (float) sline.origin().y()/10);
                     bank.setFloat("z1",   index,  (float) sline.origin().z()/10);
@@ -142,11 +134,9 @@ public class QCDDATSampleMaker extends ReconstructionEngine {
                     bank.setFloat("x3",   index,  (float) sline.end().x()/10);
                     bank.setFloat("y3",   index,  (float) sline.end().y()/10);
                     bank.setFloat("z3",   index,  (float) sline.end().z()/10);
-
                 }
-                if(hits.get(i).get(j).getDetector()==DetectorType.BMT 
-                        && hits.get(i).get(j).getType()==BMTType.C) {
-                    Arc3D sarc = hits.get(i).get(j).getStrip().getArc();
+                else if (hit.getDetector()==DetectorType.BMT && hit.getType()==BMTType.C) {
+                    Arc3D sarc = hit.getStrip().getArc();
                     bank.setFloat("x1",   index,  (float) sarc.origin().x()/10);
                     bank.setFloat("y1",   index,  (float) sarc.origin().y()/10);
                     bank.setFloat("z1",   index,  (float) sarc.origin().z()/10);
@@ -157,18 +147,18 @@ public class QCDDATSampleMaker extends ReconstructionEngine {
                     bank.setFloat("y3",   index,  (float) sarc.end().y()/10);
                     bank.setFloat("z3",   index,  (float) sarc.end().z()/10);
                 }
-            
+
                 index++;
-            }  
+            }
         }
         //bank.show();
         event.appendBanks(bank);
-        
+
         return true;
     }
 
-         
-    
+
+
     public void initConstantsTables() {
         String[] tables = new String[]{
             "/calibration/svt/status",
@@ -184,7 +174,7 @@ public class QCDDATSampleMaker extends ReconstructionEngine {
         requireConstants(Arrays.asList(tables));
         this.getConstantsManager().setVariation("default");
     }
-    
+
     public void setSvtHitBank(String bstHitBank) {
         this.svtHitBank = bstHitBank;
     }
